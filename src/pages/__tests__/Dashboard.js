bfa2d983849077facbbf6755b4ff02b591380337
Gen Ohta <myself@genohta.com>
2020-08-21 09:17:53 -0700

    remove unused imports

--- src/pages/__tests__/Dashboard.js
+++ src/pages/__tests__/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, fireEvent, screen} from '@testing-library/react'
+import {render} from '@testing-library/react'
 import Dashboard from '../Dashboard'
 import UserContext from '../../UserContext'
 
