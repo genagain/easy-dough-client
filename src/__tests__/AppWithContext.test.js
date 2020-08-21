1507abbb2d94ca367a596eca447797bb026a2b9a
Gen Ohta <myself@genohta.com>
2020-08-21 13:30:25 -0700

    refactor test

--- src/__tests__/AppWithContext.test.js
+++ src/__tests__/AppWithContext.test.js
@@ -2,11 +2,6 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import {render, fireEvent, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-// TODO uninstall history package if it's not used
-import {createMemoryHistory} from 'history'
-
-import App from '../App'
-//import UserContext from '../UserContext'
 
 import AppWithContext from '../AppWithContext'
 
@@ -20,15 +15,15 @@ function renderWithRouter(
 }
 
 test('test navigation', () => {
-	const { container } = renderWithRouter(<AppWithContext />)
+	const { getByRole } = renderWithRouter(<AppWithContext />)
 
-	const loginLink = screen.getByRole("link", { name: /login/i })
+	const loginLink = getByRole("link", { name: /login/i })
 	fireEvent.click(loginLink)
-	const header = screen.getByRole("heading", { name: /login/i })
+	const header = getByRole("heading", { name: /login/i })
 	expect(header.textContent).toMatchInlineSnapshot(`"Login"`)
 
-	const indexLink = screen.getByRole("link", { name: /easy dough/i })
+	const indexLink = getByRole("link", { name: /easy dough/i })
 	fireEvent.click(indexLink)
-	const indexText = screen.getByRole("heading", { name: /index/i })
+	const indexText = getByRole("heading", { name: /index/i })
 	expect(indexText.textContent).toMatchInlineSnapshot(`"index"`)
 })
