bb95c87bc47daa976cd87b5f6c6ba76953f69aa4
Gen Ohta <myself@genohta.com>
2020-08-21 12:14:58 -0700

    remove history from renderWithRouter

--- src/__tests__/AppWithContext.test.js
+++ src/__tests__/AppWithContext.test.js
@@ -16,10 +16,6 @@ function renderWithRouter(
 ) {
 	return {
 		...render(<BrowserRouter>{ui}</BrowserRouter>),
-		// adding `history` to the returned utilities to allow us
-		// to reference it in our tests (just try to avoid using
-		// this to test implementation details).
-		history,
 	}
 }
 
