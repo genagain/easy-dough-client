909009a2df1282023afb4e4ad3bf0fa22a8e730f
Gen Ohta <myself@genohta.com>
2020-08-21 12:16:29 -0700

    remove extra stuff in index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,14 +1,8 @@
 import React, { useState } from 'react'
-import Button from '@material-ui/core/Button';
 
 function Index() {
-  const [message, setMessage] = useState()
   return (
-    <div>
     <h1>index</h1>
-    <h2>{message}</h2>
-    <Button color="inherit" onClick={ () => setMessage('Some Message')}>Show</Button>
-    </div>
   )
 }
 
