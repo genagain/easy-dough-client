5d113654b255d4261bb639b3c42b1c94fc6c3140
Gen Ohta <myself@genohta.com>
2020-08-25 21:59:54 -0700

    add sign up route and formatted navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import UserContext from '../UserContext';
@@ -15,11 +16,12 @@ const useStyles = makeStyles((theme) => ({
           marginRight: theme.spacing(2),
         },
     title: {
-          flexGrow: 1,
+      
         },
     login : {
       'text-decoration': 'none',
-      color: 'white'
+      color: 'white',
+      padding: '1 rem'
     },
     index : {
       'text-decoration': 'none',
@@ -38,23 +40,42 @@ function NavBar() {
           <div className={classes.root}>
             <AppBar position="static">
               <Toolbar>
+         <Grid
+            justify="space-between" // Add it here :)
+            container 
+            spacing={24}
+          >
+      <Grid item xs={10}>
                 <Typography variant="h6" className={classes.title}>
                   <Link to='/' className={classes.index}>
                     Easy Dough
                   </Link>
                 </Typography>
+      </Grid>
                { accessToken ? (
                 <Button color="inherit" onClick={logout} className={classes.login}>
                   Logout
                 </Button>
                ) : (
-                <Typography variant="button">
-                 <Link to='/login' className={classes.login}>
-                  Login
-                 </Link>
-                </Typography>
+                 <>
+      <Grid item>
+                  <Typography variant="button">
+                   <Link to='/signup' className={classes.login}>
+                    Sign Up
+                   </Link>
+                  </Typography>
+      </Grid>
+      <Grid item>
+                  <Typography variant="button">
+                   <Link to='/login' className={classes.login}>
+                    Login
+                   </Link>
+                  </Typography>
+      </Grid>
+                 </>
                )
                }
+      </Grid>
               </Toolbar>
             </AppBar>
           </div>
