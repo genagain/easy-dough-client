962620c17db735d0dc5f9094e715f2ae80af5517
Gen Ohta <myself@genohta.com>
2020-09-04 20:03:21 -0700

    add TODO for later

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-// TODO refactor into one Auth form with isSignup prop, make the errors red and add component the tests using jest.fn() (hopefully I don't have to touch the page tests)
+// TODO after removing MUI, refactor into one Auth form with isSignup prop, make the errors red and add component the tests using jest.fn() (hopefully I don't have to touch the page tests) 
 function Login() {
 
   const { login } = useContext(UserContext)
