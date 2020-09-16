33478ae02a08d93e5393360081ae3012a78bdf9e
Gen Ohta <myself@genohta.com>
2020-09-15 19:48:57 -0700

    remove commented out code

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,23 +2,6 @@ import  React, { useState, useContext } from 'react'
 import { useHistory } from 'react-router-dom';
 import UserContext from '../UserContext';
 
-    //<Container maxWidth="xs" className={classes.container}>
-      //<Typography variant="h4" className={classes.title}>
-        //Login
-      //</Typography>
-      //<Typography variant="body1" className={classes.title}>
-        //{error}
-      //</Typography>
-      //<form className={classes.form} onSubmit={handleLogin}>
-        //<TextField data-testid="textField-email" label="Email" className={classes.field} onInput={ event => setEmail(event.target.value) } />
-        //<TextField data-testid="textField-password" label="Password" type="password" className={classes.field} onInput={ event => setPassword(event.target.value) }/>
-        //<div className={classes.buttons}>
-          //<Button variant="contained" type="submit" color="primary">Log In</Button>
-          //<Button variant="contained" color="secondary" onClick={handleDemoLogin}>Demo Log In</Button>
-        //</div>
-      //</form>
-    //</Container>
-//
 // TODO after removing MUI, refactor into one Auth form with isSignup prop, make the errors red and add component the tests using jest.fn() (hopefully I don't have to touch the page tests) 
 function Login() {
 
