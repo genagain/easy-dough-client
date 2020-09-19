0f04bd4ee394588e54ada6f1f5485ec0128a5fbf
Gen Ohta <myself@genohta.com>
2020-09-19 15:32:37 -0700

    rename assets directory to styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import './assets/main.css';
+import './styles/main.css';
 import './index.css';
 import AppWithContext from './AppWithContext';
 
