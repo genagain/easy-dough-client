692987c6079f82f68ef40ea1162fbdfbe9965e09
Gen Ohta <myself@genohta.com>
2020-09-19 21:09:30 -0700

    revert larger screen changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,43 +7,42 @@ function NavBar() {
 
     const [showAuth, setShowAuth] = useState(false)
 
-    //TODO iron out showAuth for screen sizes larger than md
     function toggleShowAuth() {
       setShowAuth(!showAuth)
     }
 
     return (
       <div className="container mx-auto px-6 py-4">
-        <div className="flex flex-col md:flex-row">
-          <Link to='/' className="text-6xl md:text-4xl">
+        <div className="flex flex-col">
+        <div className="flex justify-between">
+          <Link to='/' className="text-6xl">
             <h1>Easy Dough</h1>
           </Link>
-        <div className="md:hidden">
         <button type="button" onClick={toggleShowAuth} className={ showAuth ? "focus:text-gray-600" : "focus:outline-none"} aria-label="toggle menu">
                   <svg viewBox="0 0 24 24" className="h-16 w-16 fill-current">
                     <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
                   </svg>
                 </button>
         </div>
-        <div className="md:mx-6">
-      { accessToken ? 
+          { showAuth ?
+          accessToken ? 
            (
-              <Link to='/' onClick={logout} className="text-4xl md:text-2xl">
+              <Link to='/' onClick={logout} className="text-4xl">
                Logout
              </Link>
            ):
            (
              <>
-               <Link to='/signup' className="text-4xl md:text-2xl md:mx-4 ">
+               <Link to='/signup' className="text-4xl">
                  Sign Up
                </Link>
-               <Link to='/login' className="text-4xl md:text-2xl md:mx-4">
+               <Link to='/login' className="text-4xl">
                  Login
                </Link>
              </>
            )
-      }
-        </div>
+            : null
+          }
         </div>
       </div>
               );
