b5dce9882ff1fafebc0cc259c703b508600ed719
Gen Ohta <myself@genohta.com>
2020-09-19 21:13:09 -0700

    disable toggling auth links for now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,13 +18,13 @@ function NavBar() {
           <Link to='/' className="text-6xl">
             <h1>Easy Dough</h1>
           </Link>
-        <button type="button" onClick={toggleShowAuth} className={ showAuth ? "focus:text-gray-600" : "focus:outline-none"} aria-label="toggle menu">
+        <button type="button" className={ showAuth ? "focus:text-gray-600" : "focus:outline-none"} aria-label="toggle menu">
                   <svg viewBox="0 0 24 24" className="h-16 w-16 fill-current">
                     <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
                   </svg>
                 </button>
         </div>
-          { showAuth ?
+          {
           accessToken ? 
            (
               <Link to='/' onClick={logout} className="text-4xl">
@@ -41,7 +41,6 @@ function NavBar() {
                </Link>
              </>
            )
-            : null
           }
         </div>
       </div>
