17ca4ea4242a836c369d6bddcda3e51d2966e0c3
Gen Ohta <myself@genohta.com>
2020-09-20 11:24:57 -0700

    Position and format css (#13)
    * add base styles for heading tags
    
    * add base color for anchor tags
    
    * move navlinks to the right
    
    * remove accessToken for now
    
    * remove base styles for headers
    
    * add hamburger menu
    
    * conditionally show auth links when the hamburger is clicked
    
    * make elements horizontal
    
    * revert larger screen changes
    
    * disable toggling auth links for now
    
    * hide hamburger menu for medium and larger screens
    
    * make text smaller
    
    * make navbar horizontal
    
    * separate auth links from index link
    
    * add space between auth links
    
    * only add space between links for md screens
    
    * toggle auth links with css
    
    * make hamburger menu turn a lighter gray when toggled
    
    * actually get breakpoints to be responsive
    
    * add TODO for later

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,53 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import UserContext from '../UserContext';
 
 function NavBar() {
     const { accessToken, logout } = useContext(UserContext)
 
+    const [showAuth, setShowAuth] = useState(false)
+
+    function toggleShowAuth() {
+      setShowAuth(!showAuth)
+    }
+
+    // TODO figure out why the md breakpoints aren't working as expected using the lg ones seem a bit too large
     return (
-      <>
-                  <Link to='/'>
-                    <h1>Easy Dough</h1>
-                  </Link>
-               { accessToken ? 
-                 (
-                    <Link to='/' onClick={logout}>
-                     Logout
-                   </Link>
-                 ):
-                 (
-                   <>
-                   <Link to='/signup'>
-                     Sign Up
-                   </Link>
-                   <Link to='/login'>
-                     Login
-                   </Link>
-                   </>
-                 )
-               }
-      </>
+      <div className="container mx-auto px-6 py-4">
+        <div className="flex flex-col lg:flex-row">
+        <div className="flex justify-between">
+          <Link to='/' className="text-6xl lg:text-4xl">
+            <h1>Easy Dough</h1>
+          </Link>
+        <div className="lg:hidden">
+        <button type="button" onClick={toggleShowAuth} className={ showAuth ? "focus:text-gray-500" : "focus:outline-none"} aria-label="toggle menu">
+                  <svg viewBox="0 0 24 24" className="h-16 w-16 fill-current">
+                    <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                  </svg>
+                </button>
+        </div>
+        </div>
+        <div className="lg:flex-grow"></div>
+          {
+          accessToken ? 
+           (
+              <Link to='/' onClick={logout} className="text-4xl lg:text-2xl">
+               Logout
+             </Link>
+           ):
+           (
+             <>
+               <Link to='/signup' className={`${showAuth ? null : 'invisible'} text-4xl lg:visible lg:text-2xl lg:px-4`}>
+                 Sign Up
+               </Link>
+               <Link to='/login' className={`${showAuth ? null : 'invisible'} text-4xl lg:visible lg:text-2xl lg:px-4`}>
+                 Login
+               </Link>
+             </>
+           )
+          }
+        </div>
+      </div>
               );
 }
 
