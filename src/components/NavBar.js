05f7ed377c9e5070b7e3e557b0f8859d4e723eb6
Gen Ohta <myself@genohta.com>
2020-09-19 22:02:29 -0700

    actually get breakpoints to be responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,12 +13,12 @@ function NavBar() {
 
     return (
       <div className="container mx-auto px-6 py-4">
-        <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col lg:flex-row">
         <div className="flex justify-between">
-          <Link to='/' className="text-6xl md:text-4xl">
+          <Link to='/' className="text-6xl lg:text-4xl">
             <h1>Easy Dough</h1>
           </Link>
-        <div className="md:hidden">
+        <div className="lg:hidden">
         <button type="button" onClick={toggleShowAuth} className={ showAuth ? "focus:text-gray-500" : "focus:outline-none"} aria-label="toggle menu">
                   <svg viewBox="0 0 24 24" className="h-16 w-16 fill-current">
                     <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
@@ -26,20 +26,20 @@ function NavBar() {
                 </button>
         </div>
         </div>
-        <div className="md:flex-grow"></div>
+        <div className="lg:flex-grow"></div>
           {
           accessToken ? 
            (
-              <Link to='/' onClick={logout} className="text-4xl md:text-2xl">
+              <Link to='/' onClick={logout} className="text-4xl lg:text-2xl">
                Logout
              </Link>
            ):
            (
              <>
-               <Link to='/signup' className={`${showAuth ? null : 'invisible'} text-4xl md:visible md:text-2xl md:px-4`}>
+               <Link to='/signup' className={`${showAuth ? null : 'invisible'} text-4xl lg:visible lg:text-2xl lg:px-4`}>
                  Sign Up
                </Link>
-               <Link to='/login' className={`${showAuth ? null : 'invisible'} text-4xl md:visible md:text-2xl md:px-4`}>
+               <Link to='/login' className={`${showAuth ? null : 'invisible'} text-4xl lg:visible lg:text-2xl lg:px-4`}>
                  Login
                </Link>
              </>
