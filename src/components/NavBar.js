104e98c701d9fc0dec12855690628b9292165314
Gen Ohta <myself@genohta.com>
2020-09-19 21:40:55 -0700

    toggle auth links with css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,7 +19,7 @@ function NavBar() {
             <h1>Easy Dough</h1>
           </Link>
         <div className="md:hidden">
-        <button type="button" className={ showAuth ? "focus:text-gray-600" : "focus:outline-none"} aria-label="toggle menu">
+        <button type="button" onClick={toggleShowAuth} className={ showAuth ? "focus:text-gray-600" : "focus:outline-none"} aria-label="toggle menu">
                   <svg viewBox="0 0 24 24" className="h-16 w-16 fill-current">
                     <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
                   </svg>
@@ -36,10 +36,10 @@ function NavBar() {
            ):
            (
              <>
-               <Link to='/signup' className="text-4xl md:text-2xl md:px-4">
+               <Link to='/signup' className={`${showAuth ? null : 'invisible'} text-4xl md:visible md:text-2xl md:px-4`}>
                  Sign Up
                </Link>
-               <Link to='/login' className="text-4xl md:text-2xl md:px-4">
+               <Link to='/login' className={`${showAuth ? null : 'invisible'} text-4xl md:visible md:text-2xl md:px-4`}>
                  Login
                </Link>
              </>
