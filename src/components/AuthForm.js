4965db6628bdd59c49088d874595d77a4dde4a79
Gen Ohta <myself@genohta.com>
2020-09-20 21:31:08 -0700

    change all of the py and px instance to p

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -3,22 +3,21 @@ import React from 'react'
 function AuthForm({onSubmit, isSignup, fieldSetters}) {
   const {setFirstname, setLastname, setEmail, setPassword, setPasswordConfirmation} = fieldSetters
 
-  // TODO change py-6 px-6 to p-6
   return (
     <div className="pt-4">
     <form onSubmit={onSubmit} data-testid="form">
     { isSignup ?
       <div className="flex flex-col lg:flex-row lg:my-6 lg:space-x-10">
-        <input type="text" name="firstname" placeholder="First Name" onChange={ e => setFirstname(e.target.value)} className= "my-6 py-6 px-6 text-4xl lg:p-2 lg:text-base" />
-        <input type="text" name="lastname" placeholder="Last Name" onChange={e => setLastname(e.target.value)} className="my-6 py-6 px-6 text-4xl lg:p-2 lg:text-base"/>
+        <input type="text" name="firstname" placeholder="First Name" onChange={ e => setFirstname(e.target.value)} className= "my-6 p-6 text-4xl lg:p-2 lg:text-base" />
+        <input type="text" name="lastname" placeholder="Last Name" onChange={e => setLastname(e.target.value)} className="my-6 p-6 text-4xl lg:p-2 lg:text-base"/>
       </div>
       : null }
-      <input type="text" name="email" placeholder="Email" onChange={e => setEmail(e.target.value) } className="block w-full my-6 py-6 px-6 text-4xl lg:p-2 lg:text-base"/>
-      <input type="password" name="password" placeholder="Password" onChange={e => setPassword(e.target.value) } className="block w-full my-6 py-6 px-6 text-4xl lg:p-2 lg:text-base"/>
+      <input type="text" name="email" placeholder="Email" onChange={e => setEmail(e.target.value) } className="block w-full my-6 p-6 text-4xl lg:p-2 lg:text-base"/>
+      <input type="password" name="password" placeholder="Password" onChange={e => setPassword(e.target.value) } className="block w-full my-6 p-6 text-4xl lg:p-2 lg:text-base"/>
     { isSignup ?
-      <input type="password" name="password_confirmation" placeholder="Confirm Password" onChange={e => setPasswordConfirmation(e.target.value)} className="block w-full my-6 py-6 px-6 text-4xl lg:p-2 lg:text-base"/>
+      <input type="password" name="password_confirmation" placeholder="Confirm Password" onChange={e => setPasswordConfirmation(e.target.value)} className="block w-full my-6 p-6 text-4xl lg:p-2 lg:text-base"/>
       : null }
-      <button type="submit" className="block m-auto my-5 py-6 px-6 border-2 border-gray-500 rounded-lg text-4xl">{ isSignup ? 'Sign Up' : 'Log In'}</button>
+      <button type="submit" className="block m-auto my-5 p-6 border-2 border-gray-500 rounded-lg text-4xl">{ isSignup ? 'Sign Up' : 'Log In'}</button>
     </form>
     </div>
   )
