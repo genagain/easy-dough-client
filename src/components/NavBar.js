40d285d6c775dc41ce32a3a13fae7e04fa2d34cf
Gen Ohta <myself@genohta.com>
2020-09-20 17:08:01 -0700

    add padding top to hamburger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,7 +19,7 @@ function NavBar() {
           <Link to='/' className="text-6xl lg:text-4xl">
             <h1>Easy Dough</h1>
           </Link>
-        <div className="lg:hidden">
+        <div className="pt-3 lg:hidden">
         <button type="button" onClick={toggleShowAuth} className={ showAuth ? "focus:text-gray-500" : "focus:outline-none"} aria-label="toggle menu">
                   <svg viewBox="0 0 24 24" className="h-16 w-16 fill-current">
                     <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
