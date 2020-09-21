153f467da4b81262b3b50445f3421c95f2cd906a
Gen Ohta <myself@genohta.com>
2020-09-21 15:45:18 -0700

    center signup title again

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -43,8 +43,8 @@ function Signup() {
   }
 
   return (
-    <div className="flex flex-col items-center">
-    <h4 className="text-6xl lg:text-4xl">Sign Up</h4>
+    <div className="flex flex-col">
+    <h4 className="m-auto text-6xl lg:text-4xl">Sign Up</h4>
     {error ? <p>{error}</p> : null}
     <AuthForm isSignup onSubmit={handleSubmit} fieldSetters={{setFirstname, setLastname, setEmail, setPassword, setPasswordConfirmation}}/>
     </div>
