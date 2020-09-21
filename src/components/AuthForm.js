3e0b5c6a35918e2dddfed25f65d929831a0184b6
Gen Ohta <myself@genohta.com>
2020-09-20 17:20:53 -0700

    add padding and spacing to sign up form input fields

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -7,15 +7,15 @@ function AuthForm({onSubmit, isSignup, fieldSetters}) {
     <div className="pt-4">
     <form onSubmit={onSubmit} data-testid="form">
     { isSignup ?
-      <>
-        <input type="text" name="firstname" placeholder="First Name" onChange={ e => setFirstname(e.target.value)}/>
-        <input type="text" name="lastname" placeholder="Last Name" onChange={e => setLastname(e.target.value)}/>
-      </>
+      <div className="my-10 space-x-10">
+        <input type="text" name="firstname" placeholder="First Name" onChange={ e => setFirstname(e.target.value)} className= "py-6 text-4xl" />
+        <input type="text" name="lastname" placeholder="Last Name" onChange={e => setLastname(e.target.value)} className="py-6 text-4xl"/>
+      </div>
       : null }
-      <input type="text" name="email" placeholder="Email" onChange={e => setEmail(e.target.value) } className="block my-10 py-6 text-4xl"/>
-      <input type="password" name="password" placeholder="Password" onChange={e => setPassword(e.target.value) } className="block my-10 py-6 text-4xl"/>
+      <input type="text" name="email" placeholder="Email" onChange={e => setEmail(e.target.value) } className="block w-full my-10 py-6 text-4xl"/>
+      <input type="password" name="password" placeholder="Password" onChange={e => setPassword(e.target.value) } className="block w-full my-10 py-6 text-4xl"/>
     { isSignup ?
-      <input type="password" name="password_confirmation" placeholder="Confirm Password" onChange={e => setPasswordConfirmation(e.target.value)}/>
+      <input type="password" name="password_confirmation" placeholder="Confirm Password" onChange={e => setPasswordConfirmation(e.target.value)} className="block w-full my-10 py-6 text-4xl"/>
       : null }
       <button type="submit" className="block m-auto my-10 py-6 py-3 text-4xl">{ isSignup ? 'Sign Up' : 'Log In'}</button>
     </form>
