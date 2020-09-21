ed42d3ea6fd16f55170a9ec76f1dc86a614f3fbb
Gen Ohta <myself@genohta.com>
2020-09-20 17:07:06 -0700

    remove fill-rule attribute

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,7 +22,7 @@ function NavBar() {
         <div className="lg:hidden">
         <button type="button" onClick={toggleShowAuth} className={ showAuth ? "focus:text-gray-500" : "focus:outline-none"} aria-label="toggle menu">
                   <svg viewBox="0 0 24 24" className="h-16 w-16 fill-current">
-                    <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                    <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
                   </svg>
                 </button>
         </div>
