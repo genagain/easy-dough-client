368a9473ccc52baa4797ec655522e55b25b3863b
Gen Ohta <myself@genohta.com>
2020-09-21 16:03:57 -0700

    increase space between first and last

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -7,7 +7,7 @@ function AuthForm({onSubmit, isSignup, fieldSetters}) {
     <div className="pt-4">
     <form onSubmit={onSubmit} data-testid="form">
     { isSignup ?
-      <div className="lg:flex lg:flex-row  lg:m-auto lg:space-x-2 lg:max-w-sm">
+      <div className="lg:flex lg:flex-row  lg:m-auto lg:space-x-4 lg:max-w-sm">
         <input type="text" name="firstname" placeholder="First Name" onChange={ e => setFirstname(e.target.value)} className= "block m-auto w-6/12 my-6 p-6 text-4xl lg:m-0 lg:p-2 lg:text-lg" />
         <input type="text" name="lastname" placeholder="Last Name" onChange={e => setLastname(e.target.value)} className="block m-auto w-6/12 my-6 p-6 text-4xl lg:m-0 lg:p-2 lg:text-lg"/>
       </div>
