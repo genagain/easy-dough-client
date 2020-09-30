54ff8060b8aa542f494060eec215cdc9c8cf963a
Gen Ohta <myself@genohta.com>
2020-09-29 21:41:43 -0700

    rendering transactions in reverse chronological order

--- src/components/TransactionsTable.js
+++ src/components/TransactionsTable.js
@@ -7,14 +7,6 @@ function TransactionsTable({allTransactions}) {
     )
   }
   // TODO create a transactions row component
-/*                const { date, description, amount } = transaction*/
-                //return (
-                  //<div key={`${description}-${amount}-${date}`}>
-                    //<div key={`${date}-${description}-${amount}`}>{date}</div>
-                    //<div key={`${description}-${date}`}>{description}</div>
-                    //<div key={`${amount}-${date}`}>{amount}</div>
-                  //</div>
- 
   // Ideally not use a nested map
   return (
     <div>
@@ -24,6 +16,18 @@ function TransactionsTable({allTransactions}) {
         return (
           <div key={month}>
             <h1>{month}</h1>
+            {
+              transactions.map( transaction => {
+                const { date, description, amount } = transaction
+                return (
+                  <div key={`${description}-${amount}-${date}`}>
+                    <div key={`${date}-${description}-${amount}`}>{date}</div>
+                    <div key={`${description}-${date}`}>{description}</div>
+                    <div key={`${amount}-${date}`}>{amount}</div>
+                  </div>
+                )
+              })
+            }
           </div>
         )
       })
--- src/components/__tests__/TransactionsTable.test.js
+++ src/components/__tests__/TransactionsTable.test.js
@@ -33,25 +33,25 @@ describe('When there are transactions to render, the TransactionsTable component
     })
 
     // TODO test this rendering each cell in TransactionRow component
-/*    test('each transaction', () => {*/
-      //const expectedTransactions = [
-        //{ 'date': '2020-07-10', 'description': 'Grocery Store', 'amount': 70.00 },
-        //{ 'date': '2020-07-04', 'description': 'Wine', 'amount': 15.00 },
-        //{ 'date': '2020-06-21', 'description': 'Pizza Delivery', 'amount': 20.00 },
-        //{ 'date': '2020-06-13', 'description': 'Beer', 'amount': 10.00 }
-      //]
+    test('each transaction', () => {
+      const expectedTransactions = [
+        { 'date': '2020-07-10', 'description': 'Grocery Store', 'amount': 70.00 },
+        { 'date': '2020-07-04', 'description': 'Wine', 'amount': 15.00 },
+        { 'date': '2020-06-21', 'description': 'Pizza Delivery', 'amount': 20.00 },
+        { 'date': '2020-06-13', 'description': 'Beer', 'amount': 10.00 }
+      ]
 
-      //expectedTransactions.forEach(transaction => {
-        //const date = screen.getByText(transaction.date)
-        //expect(date).not.toBeNull()
+      expectedTransactions.forEach(transaction => {
+        const date = screen.getByText(transaction.date)
+        expect(date).not.toBeNull()
 
-        //const description = screen.getByText(transaction.description)
-        //expect(description).not.toBeNull()
+        const description = screen.getByText(transaction.description)
+        expect(description).not.toBeNull()
 
-        //const amount = screen.getByText(`${transaction.amount}`)
-        //expect(amount).not.toBeNull()
-      //})
-    //})
+        const amount = screen.getByText(`${transaction.amount}`)
+        expect(amount).not.toBeNull()
+      })
+    })
 
     test('each transaction by month in reverse chronological order', () => {
         const months = screen.getAllByRole('heading', { name: /(june)|(july)/i })
@@ -59,5 +59,6 @@ describe('When there are transactions to render, the TransactionsTable component
         expect(months[0]).toHaveTextContent('July')
         expect(months[1]).toHaveTextContent('June')
     })
+        // TODO expect that each month renders the children under it
   })
 })
