4ec3921cc53c6279868ce2e8ceb0203cfeb103b2
Gen Ohta <myself@genohta.com>
2020-09-29 20:46:22 -0700

    rendering transactions

--- src/components/TransactionsTable.js
+++ src/components/TransactionsTable.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
-function TransactionsTable() {
+function TransactionsTable({transactions}) {
+  if (transactions.length === 0) {
+    return (
+      <p>Looks like we don't have any transactions to show yet</p>
+    )
+  }
+
   return (
-    <p>Looks like we don't have any transactions to show yet</p>
+    <div>
+      {
+        transactions.map(transaction => {
+          const { date, description, amount } = transaction
+          return (
+            <div key={`${description}-${amount}-${date}`}>
+              <div key={`${date}-${description}-${amount}`}>{date}</div>
+              <div key={`${description}-${date}`}>{description}</div>
+              <div key={`${amount}-${date}`}>{amount}</div>
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
--- src/components/__tests__/TransactionsTable.test.js
+++ src/components/__tests__/TransactionsTable.test.js
@@ -9,3 +9,38 @@ describe('When there are no transactions to render, the TransactionsTable compon
     expect(message).not.toBeNull()
   })
 })
+
+describe('When there are transactions to render, the TransactionsTable component', () => {
+
+  describe('render', () => {
+    beforeEach(() => {
+      const transactions = [
+        { 'date': '2020-07-10', 'description': 'Grocery Store', 'amount': 70.00 },
+        { 'date': '2020-07-04', 'description': 'Wine', 'amount': 15.00 },
+        { 'date': '2020-06-21', 'description': 'Pizza Delivery', 'amount': 20.00 },
+        { 'date': '2020-06-13', 'description': 'Beer', 'amount': 10.00 },
+      ]
+      render(<TransactionsTable transactions={transactions} />)
+    })
+
+    test('each transaction', () => {
+      const expectedTransaction = [
+        { 'date': '2020-07-10', 'description': 'Grocery Store', 'amount': 70.00 },
+        { 'date': '2020-07-04', 'description': 'Wine', 'amount': 15.00 },
+        { 'date': '2020-06-21', 'description': 'Pizza Delivery', 'amount': 20.00 },
+        { 'date': '2020-06-13', 'description': 'Beer', 'amount': 10.00 }
+      ]
+
+      expectedTransaction.forEach(transaction => {
+        const date = screen.getByText(transaction.date)
+        expect(date).not.toBeNull()
+
+        const description = screen.getByText(transaction.description)
+        expect(description).not.toBeNull()
+
+        const amount = screen.getByText(`${transaction.amount}`)
+        expect(amount).not.toBeNull()
+      })
+    })
+  })
+})
