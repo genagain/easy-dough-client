5fb8172c8935e729e6f6671b0050ec43b2d9acd4
Gen Ohta <myself@genohta.com>
2020-10-02 21:45:16 -0700

    rendering transactions from a fetch call

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import TransactionsTableList from '../components/TransactionsTableList'
+import UserContext from '../UserContext'
 
 function Transactions() {
+
+  const { accessToken } = useContext(UserContext)
+  const [allTransactions, setAllTransactions] = useState([])
+
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      const apiUrl = process.env.REACT_APP_SERVER_BASE_URL
+      const params = new URLSearchParams({
+        start_date: '2020-08-22',
+        end_date: '2020-09-12',
+      })
+
+      const response = await fetch(`${apiUrl}/transactions?${params}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+          }
+        })
+
+      const transactions = await response.json()
+      setAllTransactions(transactions)
+    }
+
+    fetchTransactions()
+
+  }, [])
+
   return (
+    <>
     <h1>Transactions</h1>
+    <TransactionsTableList allTransactions={allTransactions} />
+    </>
   )
 }
 
