0626c4a84346e5361800b824e8af855a52cc2dc1
Gen Ohta <myself@genohta.com>
2020-10-02 22:49:32 -0700

    deducing the dates

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -7,13 +7,29 @@ function Transactions() {
   const { accessToken } = useContext(UserContext)
   const [allTransactions, setAllTransactions] = useState([])
 
+  function deduceDate(date, isStart = false) {
+    if (isStart) {
+      date.setMonth(date.getMonth() - 2)
+    }
+
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit'}
+    const [month, day, year] = date.toLocaleDateString('en-US', options).split('/')
+    
+    const intlDate = isStart ? [year, month, '01'] : [year, month, day]
+    return intlDate.join('-')
+  }
 
   useEffect(() => {
     const fetchTransactions = async () => {
       const apiUrl = process.env.REACT_APP_SERVER_BASE_URL
+      
+      const today = new Date(Date.now())
+      const end_date = deduceDate(today)
+      const start_date = deduceDate(today, true)
+
       const params = new URLSearchParams({
-        start_date: '2020-08-22',
-        end_date: '2020-09-12',
+        start_date: start_date,
+        end_date: end_date,
       })
 
       const response = await fetch(`${apiUrl}/transactions?${params}`,
@@ -25,13 +41,14 @@ function Transactions() {
           }
         })
 
-      const transactions = await response.json()
-      setAllTransactions(transactions)
+      if (response.ok) {
+        const transactions = await response.json()
+        setAllTransactions(transactions)
+      }
     }
 
     fetchTransactions()
-
-  }, [])
+  }, [accessToken])
 
   return (
     <>
