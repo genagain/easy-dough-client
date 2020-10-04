a4fdb821439a45902b533c2de7dced6f3d0f9d49
Gen Ohta <myself@genohta.com>
2020-10-04 15:04:45 -0700

    clean up tests

--- src/pages/__tests__/Transactions.test.js
+++ src/pages/__tests__/Transactions.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react'
 import MockDate from 'mockdate'
-
 import Transactions from '../Transactions'
 import UserContext from '../../UserContext'
 
