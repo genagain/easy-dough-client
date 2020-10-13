90accade591dd3132c8dfcb068b7a73a4c58aab8
Gen Ohta <myself@genohta.com>
2020-10-13 07:45:00 -0700

    export util functions correctly

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,14 @@
-function formatDate(date) {
+export const formatDate = function(date) {
   const options = { year: 'numeric', month: '2-digit', day: '2-digit'}
   const [month, day, year] = date.toLocaleDateString('en-US', options).split('/')
   return [year, month, day].join('-')
 }
 
-function formatAmount(amount) {
+export const formatAmount = function(amount) {
   let withoutCents = /^\d{0,3},{0,1}\d{0,3}$/
   if (withoutCents.test(amount)){
     return `${amount}.00`
   } else {
     return amount
   }
-}
-
-module.exports = {
-  formatDate,
-  formatAmount
 }
