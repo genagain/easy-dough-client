ed5eac912c29377ab23a702875e81b13f0974562
Gen Ohta <myself@genohta.com>
2020-10-14 19:25:30 -0700

    convert TransactionRow to react fragement

--- src/components/TransactionRow.js
+++ src/components/TransactionRow.js
@@ -4,12 +4,12 @@ function TransactionRow({transaction}) {
   const { id, date, description, amount } = transaction
 
   return (
-    <div key={id}>
+    <>
       <div key={`${id}-${date}`}>{date}</div>
       <div key={`${id}-${description}`}>{description}</div>
       <div key={`${id}-${amount}`}>{amount}</div>
       <button data-testid={`delete-${id}`}>Delete</button>
-    </div>
+    </>
 
   )
 }
