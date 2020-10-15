b6f460e2999d66f8a638b3337aa3fd541a933963
Gen Ohta <myself@genohta.com>
2020-10-14 19:21:48 -0700

    refactor component

--- src/components/TransactionsTable.js
+++ src/components/TransactionsTable.js
@@ -1,22 +1,16 @@
 import React from 'react';
+import TransactionRow from './TransactionRow'
 
 function TransactionsTable({transactions}) {
-  // TODO consider creating a transactions row component
-  // TODO consider testing order of rendered transactions
   return (
         <div>
             {
-              transactions.map( transaction => {
-                const { id, date, description, amount } = transaction
-                return (
-                  <div key={`${description}-${amount}-${date}`}>
-                    <div key={`${date}-${description}-${amount}`}>{date}</div>
-                    <div key={`${description}-${date}`}>{description}</div>
-                    <div key={`${amount}-${date}`}>{amount}</div>
-                    <button data-testid={`delete-${id}`}>Delete</button>
-                  </div>
+              transactions.map( transaction => (
+                <div key={transaction.id}>
+                  <TransactionRow transaction={transaction}/>
+                </div>
                 )
-              })
+              )
             }
           </div>
   )
