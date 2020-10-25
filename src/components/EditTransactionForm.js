ea10be261f221706dbb86ae35c0ea05af71230b5
Gen Ohta <myself@genohta.com>
2020-10-24 20:35:51 -0700

    update transactions for real

--- src/components/EditTransactionForm.js
+++ src/components/EditTransactionForm.js
@@ -1,43 +1,46 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import DatePicker from 'react-datepicker'
 import UserContext from '../UserContext'
-import { convertIsoToDate } from '../utils'
+import { convertDateToIso, convertIsoToDate, formatAmount } from '../utils'
 
 function EditTransactionForm({transaction, setToggleForm}) {
-  const { id, date: isoDate, description, amount } = transaction
+  const { id, date: initialIsoDate, description: initialDescription, amount: initialAmount } = transaction
   const { accessToken, queryParams, setQueryParams } = useContext(UserContext)
-  const date = convertIsoToDate(isoDate)
+  const [date, setDate] = useState(convertIsoToDate(initialIsoDate))
+  const [description, setDescription] = useState(initialDescription)
+  const [amount, setAmount] = useState(initialAmount)
 
   // TODO same validation logic as adding transaction
   async function handleUpdate(e){
     const apiUrl = process.env.REACT_APP_SERVER_BASE_URL
     const body = {
-      'date': '2020-10-24',
-      'description': 'Something hilarious',
-      'amount': '69.00'
+      'date': convertDateToIso(date),
+      'description': description,
+      'amount': formatAmount(amount)
     }
 
-     await fetch(`${apiUrl}/transactions/${id}`,
-       {
-         method: 'PUT',
-         headers: {
-           'Content-Type': 'application/json',
-           'Authorization': `Bearer ${accessToken}`
-         },
-         body: JSON.stringify(body)
-       })
-        setToggleForm(false)
-        setQueryParams({...queryParams})
-   }
+    await fetch(`${apiUrl}/transactions/${id}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify(body)
+      }
+    )
+    setToggleForm(false)
+    setQueryParams({...queryParams})
+  }
 
   return (
     <>
       <label htmlFor="date-input">Date:</label>
-      <DatePicker id="date-input" selected={date} onChange={() => {}}/>
-      <input placeholder="Description" type="text" defaultValue={description} onChange={() => {}}/>
-      <input placeholder="Amount" type="text" defaultValue={amount} onChange={() => {}}/>
+      <DatePicker id="date-input" selected={date} onChange={date => setDate(date)}/>
+      <input placeholder="Description" type="text" defaultValue={initialDescription} onChange={e => setDescription(e.target.value)}/>
+      <input placeholder="Amount" type="text" defaultValue={initialAmount} onChange={e => setAmount(e.target.value)}/>
       <button onClick={handleUpdate}>Update</button>
-      <button onClick={() => {setToggleForm(false)}}>Cancel</button>
+      <button onClick={() => setToggleForm(false)}>Cancel</button>
     </>
   )
 }
