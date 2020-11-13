4dfb668436b330efea625bff01891b7574250f1f
Gen Ohta <myself@genohta.com>
2020-11-12 18:12:54 -0800

    add bottom margin

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,6 +13,7 @@ function NavBar() {
 
     // TODO figure out why the md breakpoints aren't working as expected using the lg ones seem a bit too large
     return (
+      <div className="border-b-2 border-gray-400 shadow-md mb-24 lg:mb-12 lg:border-b lg:shadow ">
       <div className="container mx-auto px-6 py-4 lg:px-2">
         <div className="flex flex-col lg:flex-row lg:items-center">
         <div className="flex justify-between">
@@ -53,6 +54,7 @@ function NavBar() {
            )
           }
         </div>
+      </div>
       </div>
               );
 }
