d645814fda6f54778235d2800f78b621d4fd1ff7
Gen Ohta <myself@genohta.com>
2020-11-12 18:37:22 -0800

    remove focus

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,7 +21,7 @@ function NavBar() {
             <h1>Easy Dough</h1>
           </Link>
         <div className="pt-3 lg:hidden">
-        <button type="button" onClick={toggleShowAuth} className={ showLinks ? "focus:text-gray-500" : "focus:outline-none"} aria-label="toggle menu">
+        <button type="button" onClick={toggleShowAuth} className={ showLinks ? "text-gray-500" : "outline-none"} aria-label="toggle menu">
                   <svg viewBox="0 0 24 24" className="h-16 w-16 fill-current">
                     <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
                   </svg>
