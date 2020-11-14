4bedbfc107206a9a98e16394fecb4d68ffac817a
Gen Ohta <myself@genohta.com>
2020-11-13 20:23:49 -0800

    style create transaction button

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -46,7 +46,7 @@ function AddTransactionForm({setToggleCreate}) {
   }
 
   return (
-    <div className="flex flex-col lg:flex-row lg:items-center">
+    <div className="flex flex-col m-auto w-3/4 lg:w-full lg:flex-row lg:items-center">
         { flashMessage &&
             <FlashMessage duration={5000}>
               <strong>{flashMessage}</strong>
@@ -57,16 +57,16 @@ function AddTransactionForm({setToggleCreate}) {
           {({ inputProps, focused }) => (
             <input
               id="date-input"
-              className={`${'input' + (focused ? ' -focused' : '')} m-2 p-6 text-5xl border border-gray-400 rounded lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg`}
+              className={`${'input' + (focused ? ' -focused' : '')} w-full lg:w-32 m-2 p-6 text-5xl border border-gray-400 rounded lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg`}
               {...inputProps}
             />
           )}
         </DatePicker>
         <label htmlFor="description-input" className="my-2 text-5xl lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg">Description:</label>
-        <input id="description-input" className="m-2 p-6 text-5xl border border-gray-400 rounded lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg" placeholder="Coffee" onChange={ e => { setDescription(e.target.value)}}/>
+        <input id="description-input" className="w-full lg:w-84 m-2 p-6 text-5xl border border-gray-400 rounded lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg" placeholder="Coffee" onChange={ e => { setDescription(e.target.value)}}/>
         <label htmlFor="amount-input" className="my-2 text-5xl lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg">Amount:</label>
-        <input id="amount-input" className="m-2 p-6 text-5xl border border-gray-400 rounded lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg" placeholder="15.00" type="text" onChange={ e => { setAmount(e.target.value)}}/>
-        <button className="lg:h-12 m-2 p-6 border border-gray-400 rounded-lg lg:my-2 lg:p-2 text-5xl lg:text-lg" onClick={handleAddTransaction}>Create Transaction</button>
+        <input id="amount-input" className="w-full lg:w-32 m-2 p-6 text-5xl border border-gray-400 rounded lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg" placeholder="15.00" type="text" onChange={ e => { setAmount(e.target.value)}}/>
+        <button className="w-full lg:w-48 lg:h-12 m-2 p-6 bg-blue-500 hover:bg-blue-300 text-white rounded-lg lg:my-2 lg:p-2 text-5xl lg:text-lg" onClick={handleAddTransaction}>Create Transaction</button>
     </div>
   )
 }
