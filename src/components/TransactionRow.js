c436d4d36f8ebbd9102f50d566a99a6433ba9990
Gen Ohta <myself@genohta.com>
2020-11-14 17:31:11 -0800

    make table thinner

--- src/components/TransactionRow.js
+++ src/components/TransactionRow.js
@@ -37,12 +37,12 @@ function TransactionRow({transaction}) {
       ) : (
         <div className="flex flex-row lg:items-baseline">
           <div className="flex-grow lg:flex lg:flex-row lg:flex-grow-0">
-          <div key={`${id}-${date}`} className="m-2 text-3xl lg:w-40 lg:text-lg">{formattedDate}</div>
-          <div key={`${id}-${description}`} className="m-2 text-5xl lg:w-84 lg:text-lg">{description}</div>
+          <div key={`${id}-${date}`} className="m-2 text-3xl lg:m-2 lg:w-40 lg:text-base">{formattedDate}</div>
+          <div key={`${id}-${description}`} className="m-2 lg:m-2 text-5xl lg:w-84 lg:text-base">{description}</div>
           </div>
-           <div key={`${id}-${amount}`} className="mx-2 my-4 text-5xl lg:w-144 lg:text-lg">{amount}</div>
-           <button className="mx-2 my-4 px-4 py-2 lg:w-16 h-20 bg-blue-800 text-white hover:bg-blue-700 rounded-lg text-4xl lg:text-lg lg:px-2 lg:h-auto" data-testid={`edit-${id}`} onClick={() => setToggleForm(true)}>Edit</button>
-           <button className="mx-2 my-4 px-4 py-2 lg:w-20 h-20 border border-blue-800 text-blue-800 hover:border-blue-700 hover:text-blue-700 rounded-lg text-4xl lg:text-lg lg:px-2 lg:h-auto" data-testid={`delete-${id}`} onClick={() => setToggleModal(true)}>Delete</button>
+           <div key={`${id}-${amount}`} className="mx-2 my-4 lg:m-2 l text-5xl lg:w-144 lg:text-base">{amount}</div>
+           <button className="mx-2 my-4 px-4 py-2 lg:m-2 lg:w-16 h-20 bg-blue-800 text-white hover:bg-blue-700 rounded-lg text-4xl lg:text-base lg:px-2 lg:h-auto" data-testid={`edit-${id}`} onClick={() => setToggleForm(true)}>Edit</button>
+           <button className="mx-2 my-4 px-4 py-2 lg:m-2 lg:w-20 h-20 border border-blue-800 text-blue-800 hover:border-blue-700 hover:text-blue-700 rounded-lg text-4xl lg:text-base lg:px-2 lg:h-auto" data-testid={`delete-${id}`} onClick={() => setToggleModal(true)}>Delete</button>
         </div>
       )
     }
@@ -51,8 +51,8 @@ function TransactionRow({transaction}) {
         <h1 className="p-6 text-5xl lg:text-2xl">Are you sure you want to delete this transaction?</h1>
           <div className="m-auto w-3/4 lg:w-1/2">
           <div className="flex space-x-10">
-            <button className="m-2 p-6 h-28 w-56 lg:w-24 bg-blue-800 text-white hover:bg-blue-700 rounded-lg lg:my-2 lg:px-4 lg:py-2 text-5xl lg:text-lg" onClick={handleDelete}>Yes</button>
-            <button className="m-2 p-6 h-28 w-56 lg:w-24 border border-blue-800 text-blue-800 hover:border-blue-700 hover:text-blue-700 rounded-lg lg:my-2 lg:px-4 lg:py-2 text-5xl lg:text-lg" onClick={() => setToggleModal(false)}>No</button>
+            <button className="m-2 p-6 h-28 w-56 lg:w-24 bg-blue-800 text-white hover:bg-blue-700 rounded-lg lg:my-2 lg:px-4 lg:py-2 text-5xl lg:text-base" onClick={handleDelete}>Yes</button>
+            <button className="m-2 p-6 h-28 w-56 lg:w-24 border border-blue-800 text-blue-800 hover:border-blue-700 hover:text-blue-700 rounded-lg lg:my-2 lg:px-4 lg:py-2 text-5xl lg:text-base" onClick={() => setToggleModal(false)}>No</button>
           </div>
           </div>
         </div>
--- src/components/TransactionsTableList.js
+++ src/components/TransactionsTableList.js
@@ -17,7 +17,7 @@ function TransactionsTableList({allTransactions}) {
         const { month, transactions } = monthsTransactions
         return (
           <div key={month} role="list" title={month} className="m-auto w-10/12 lg:max-w-6xl">
-            <h1 className="px-1 pt-6 pb-3 text-6xl lg:text-3xl">{month}</h1>
+            <h1 className="px-1 pt-6 pb-3 text-6xl lg:text-2xl">{month}</h1>
             <div key={`transactions-table-${month}`}>
               <TransactionsTable transactions={transactions} />
             </div>
