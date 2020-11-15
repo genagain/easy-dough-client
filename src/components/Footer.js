1143376b4ec194ac7c464ed78cd88086c44e7441
Gen Ohta <myself@genohta.com>
2020-11-14 19:55:41 -0800

    make login page margin longer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,13 @@ import { faGrin } from '@fortawesome/free-solid-svg-icons'
 
 function Footer() {
   return (
-    <>
-    <a href="https://github.com/genagain" target="blank" title="Github Profile"><FontAwesomeIcon icon={faGithubSquare} /></a>
-    <a href="https://www.linkedin.com/in/genohta" target="blank" title="Linkedin Profile"><FontAwesomeIcon icon={faLinkedin} /></a>
-    <a href="http://genohta.com/" target="blank" title="Personal Site"><FontAwesomeIcon icon={faGrin} /></a>
-    </>
+    <div className="mt-56 lg:mt-24 h-96 border-t-2 border-gray-600 lg:border-t">
+      <div className="m-auto w-1/2">
+        <a href="https://github.com/genagain" target="blank" title="Github Profile" className="text-8xl"><FontAwesomeIcon icon={faGithubSquare} /></a>
+        <a href="https://www.linkedin.com/in/genohta" target="blank" title="Linkedin Profile" className="text-8xl"><FontAwesomeIcon icon={faLinkedin} /></a>
+        <a href="http://genohta.com/" target="blank" title="Personal Site" className="text-8xl"><FontAwesomeIcon icon={faGrin} /></a>
+      </div>
+    </div>
   )
 }
 
