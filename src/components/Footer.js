d04234942ced403fcb8342410661fed44d6d5bd0
Gen Ohta <myself@genohta.com>
2020-11-14 20:10:23 -0800

    mobile footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,12 @@ import { faGrin } from '@fortawesome/free-solid-svg-icons'
 function Footer() {
   return (
     <div className="mt-56 lg:mt-24 h-96 border-t-2 border-gray-600 lg:border-t">
-      <div className="m-auto w-1/2">
-        <a href="https://github.com/genagain" target="blank" title="Github Profile" className="text-8xl"><FontAwesomeIcon icon={faGithubSquare} /></a>
-        <a href="https://www.linkedin.com/in/genohta" target="blank" title="Linkedin Profile" className="text-8xl"><FontAwesomeIcon icon={faLinkedin} /></a>
-        <a href="http://genohta.com/" target="blank" title="Personal Site" className="text-8xl"><FontAwesomeIcon icon={faGrin} /></a>
+      <div className="mt-24 m-auto w-3/4">
+        <div className="flex justify-evenly">
+          <a href="https://github.com/genagain" target="blank" title="Github Profile" className="text-8xl text-blue-800 hover:text-blue-700"><FontAwesomeIcon icon={faGithubSquare} /></a>
+          <a href="https://www.linkedin.com/in/genohta" target="blank" title="Linkedin Profile" className="text-8xl text-blue-800 hover:text-blue-700"><FontAwesomeIcon icon={faLinkedin} /></a>
+          <a href="http://genohta.com/" target="blank" title="Personal Site" className="text-8xl text-blue-800 hover:text-blue-700"><FontAwesomeIcon icon={faGrin} /></a>
+        </div>
       </div>
     </div>
   )
