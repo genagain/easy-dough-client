23cd5359ac1c3e062c0e0fb0a7343bf334ac49d6
Gen Ohta <myself@genohta.com>
2020-11-17 20:59:22 -0800

    create screen height classes

--- tailwind.js
+++ tailwind.js
@@ -360,6 +360,9 @@ module.exports = {
     minHeight: {
       '0': '0',
       full: '100%',
+      'screen-1/4': '25vh',
+      'screen-1/2': '50vh',
+      'screen-3/4': '75vh',
       screen: '100vh',
     },
     minWidth: {
