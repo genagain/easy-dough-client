1f8b027b96e927a97230cbf89a1d2c6fa67acded
Gen Ohta <myself@genohta.com>
2020-11-26 13:09:07 -0800

    configure react plaid link

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useContext, useState, useEffect, useCallback } from 'react';
+import { usePlaidLink } from 'react-plaid-link';
+import UserContext from '../UserContext'
 
 function Settings() {
+  const { accessToken } = useContext(UserContext)
+
+  const onSuccess = useCallback((publicToken, metadata) => {
+    console.log('success!')
+    console.log('publicToken', publicToken)
+  }, []);
+
+  // TODO ideally make this less hacky
+  const [plaidLinkConfig, setPlaidLinkConfig] = useState({ token: 'link-sandbox', onSuccess })
+
+  useEffect(() => {
+    const fetchPlaidLinkToken = async () => {
+      const apiUrl = process.env.REACT_APP_SERVER_BASE_URL
+      const response = await fetch(`${apiUrl}/auth/create_link_token`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+          }
+        }
+      ).then(res => res.json())
+      .then(json => {
+        const config = {
+          token: json['link_token'],
+          onSuccess
+        }
+        setPlaidLinkConfig(config)
+      })
+    }
+
+    fetchPlaidLinkToken()
+  }, [accessToken])
+
+  console.log(plaidLinkConfig)
+  const { open, ready, error } = usePlaidLink(plaidLinkConfig);
   return (
-    <h1>Settings</h1>
+    <>
+      <h1>Settings</h1>
+      <button onClick={() => open()} disabled={!ready}>Click Me</button>
+    </>
   )
 }
 
