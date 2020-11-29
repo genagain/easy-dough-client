b50b0241b5fcd920b6aea18561e45095d703925e
Gen Ohta <myself@genohta.com>
2020-11-28 16:08:20 -0800

    use accounts table in banks list component

--- src/components/BanksList.js
+++ src/components/BanksList.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import AccountsTable from './AccountsTable'
 
 function BanksList({allBanks}) {
   return (
     <div>
     {
       allBanks.map(bank => {
-        const { name, logo } = bank
+        const { name, logo, accounts } = bank
         return (
           <div key={name}>
             <h1 key={`${name}-name`}>{name}</h1>
             <img key={`${name}-logo`} src={`data:image/png;base64,${logo}`} alt={`${name}'s logo`} />
+            <AccountsTable accounts={accounts} />
           </div>
         )
       })
