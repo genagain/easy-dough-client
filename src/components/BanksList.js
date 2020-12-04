3522b9b10e2e49162cc5589fe2254821d9b4f34c
Gen Ohta <myself@genohta.com>
2020-12-04 13:26:50 -0800

    Delete banks (#29)
    * render modal
    
    * fix rendering error
    
    * handle delete
    
    * refetch banks
    
    * style delete button
    
    * style delete modal
    
    * center delete row modal

--- src/components/BanksList.js
+++ src/components/BanksList.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import Modal from 'react-modal';
+import UserContext from '../UserContext'
 import AccountsTable from './AccountsTable'
 
-function BanksList({allBanks}) {
+function BanksList({allBanks, refetch, setRefetch}) {
+  const { accessToken } = useContext(UserContext)
+  const [toggleModal, setToggleModal] = useState(false)
+
+  Modal.setAppElement(document.getElementById('bank-list'))
+
+  async function deleteBankAccounts(id) {
+    const apiUrl = process.env.REACT_APP_SERVER_BASE_URL
+    const response = await fetch(`${apiUrl}/bank_accounts/${id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`,
+        }
+      })
+
+    if(response.ok) {
+      setToggleModal(false)
+      setRefetch(!refetch)
+    }
+  }
+
   if (allBanks.length === 0) {
     return (
       <p className="py-6 text-5xl lg:text-lg lg:max-w-6xl">Looks like you haven't added any banks yet. Please add a bank.</p>
@@ -9,18 +33,28 @@ function BanksList({allBanks}) {
   }
 
   return (
-    <div>
+    <div id='bank-list'>
     {
       allBanks.map(bank => {
-        const { name, logo, accounts } = bank
+        const { id, name, logo, accounts } = bank
         return (
-          <div key={name} className="py-6 lg:py-3">
+          <div key={id} className="py-6 lg:py-3">
             <div key={name} className="pb-6 lg:pb-3">
               <div className="flex flex-row items-center space-x-6 lg:space-x-3">
                 <img key={`${name}-logo`} className="h-20 w-20 lg:h-8 lg:w-8" src={`data:image/png;base64,${logo}`} alt={`${name}'s logo`} />
                 <h1 key={`${name}-name`} className="text-5xl lg:text-2xl">{name}</h1>
+                <button className="p-6 lg:p-2 lg:m-2 rounded-lg text-white bg-red-800 hover:bg-red-700 text-4xl lg:text-sm" onClick={() => setToggleModal(true)}>Delete</button>
               </div>
             </div>
+            <Modal className="m-10 p-10 bg-white h-auto border border-gray-400 rounded lg:m-20" isOpen={toggleModal}>
+              <div className="m-auto w-3/4 lg:w-1/2 flex flex-col items-center">
+                <h1 className="p-6 text-5xl lg:text-2xl">Are you sure you want to delete this bank?</h1>
+                <div className="flex flex-row space-x-10">
+                  <button className="m-2 p-6 h-28 w-56 lg:w-24 bg-blue-800 text-white hover:bg-blue-700 rounded-lg lg:my-2 lg:px-4 lg:py-2 text-5xl lg:text-base" onClick={() => deleteBankAccounts(id)}>Yes</button>
+                  <button className="m-2 p-6 h-28 w-56 lg:w-24 border border-blue-800 text-blue-800 hover:border-blue-700 hover:text-blue-700 rounded-lg lg:my-2 lg:px-4 lg:py-2 text-5xl lg:text-base" onClick={() => setToggleModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal>
             <AccountsTable accounts={accounts} />
           </div>
         )
--- src/components/__tests__/BankList.test.js
+++ src/components/__tests__/BankList.test.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import BanksList from '../BanksList'
+import UserContext from '../../UserContext'
 
 describe('When there are no banks to render, the BanksList component', () => {
   describe('renders', () => {
     beforeEach(() => {
-      render(<BanksList allBanks={[]}/>)
+      render(<UserContext.Provider value={{}}><BanksList allBanks={[]}/></UserContext.Provider>)
     })
 
     test('a prompt for the user to add a bank', () => {
@@ -14,6 +15,7 @@ describe('When there are no banks to render, the BanksList component', () => {
     })
   })
 })
+
 describe('When there are banks to render, the BanksList component', () => {
   describe('renders', () => {
     beforeEach(() => {
@@ -46,7 +48,7 @@ describe('When there are banks to render, the BanksList component', () => {
            ]
         }
       ]
-      render(<BanksList allBanks={allBanks}/>)
+      render(<UserContext.Provider value={{}}><BanksList allBanks={allBanks}/></UserContext.Provider>)
     })
 
     test('the name for each bank in alphabetical order', () => {
@@ -62,6 +64,66 @@ describe('When there are banks to render, the BanksList component', () => {
       expect(logos[0].alt).toEqual("Ally's logo")
       expect(logos[1].alt).toEqual("Navy Federal Credit Union's logo")
     })
+
+   test('the delete buttons for each bank', () => {
+     const deleteButtons = screen.getAllByRole('button', { name: /delete/i })
+     expect(deleteButtons).toHaveLength(2)
+   })
   })
 })
 
+describe("When there are banks to render, clicking the BanksList component's", () => {
+  describe('renders', () => {
+    beforeEach(() => {
+      const allBanks = [
+        {
+            'name': 'Ally',
+            'logo': "iVBORw0KGgoAAAANSUhEUgAAAJgAAACYCAMAAAAvHNATAAAARVBMVEVHcExhKWP//v9gKWJgKWJhKmNiKmRhKmNlLWVgKWL9//1jJ2NYJVi2n7bf0N9gNmDKt8r06fRxS3GDYYOObo+ojaibfptk/Qt4AAAACnRSTlMA+P//5HhLoCDG3GoG3QAACvtJREFUeNrNXIeCpCAMvYkdFcuM/v+nHlVBEsQpd7J7tztlnWcSXgqBP38+HXVZVFXTNHn+eOS5+KWqirL+8z9HXVRNnhEjb6rif8Cry0pgeogvasjX8urfyk6BSh0C3L9SoBLVlZH/Xql10WRvjean2Orqkb09HlV9R1i/g1ZX2RfG16F9B9b3oRWP7GvjUXyPtprsq6Mpb6bF7+qzzLMfjPxjoRXZj8ZnllY32c9GU99NjZ+rs8h+PN5UZ9ps7LLOGeLhpdn5DVxta4DYRxLIMD9f69KPcvT9sj7neRDwxMvtr5AFZt+6EBWmted8AjkYAyZ/iH8TH5fnLF5Pg9Z8aTq2aghUz95gEmgYk8D0TybBwTQus5CtfO+Zcq9NzjqP4JKoJi2nHQpzHqunJr4IuWXncsvrT/RoYQkbG1ZuxaQxaCXqLw/sNL6G7tzamm/gMrD0JwfAHFjmB1+H7nt2RvKEC0t/Wxsz0DROR61p0Ko3ecL86Lonhw2XBuLJzWgQLGTzVv6StvY5soLgiW7uwbMm2B5uz22vMO9947wja9/1ASVuXln30lq0n8b8D2fhU7vhwbRmJ7xWvkUUkiJ6MxFDPDQ6a3daaIpr3iYNbEIKcc18o08GIQ7/l8PT+u+mZxd1VM3lgEIS/XOC3a5JNSI/XBZZ4m6quGpgAtg6gZlwjBQMbXSWR6Afoo6gvOqJugVcZwinVh8amaGQUTiCt3xThU7HBay8HAbFpHXQH2zSAsspO7L2CpuVNK792omTEnmfkZlWJwbsUSbPSGFfy+4aAWNPCkdIaJo2hsjcbFIpX9q9QxPopxGkitrbhuzSzKwfiLxe0x7GEAQKKU/vjh36SJD2qNMsv5sn2Bw0hFIJ7T7UcOgE1gidIfZfI28b+M5flEkRQDANmxBc+AAaWR0TmIrTZbDabyRE6pFFzf7whJE7F2bWpYqsPsY5Mp6A7S6D20d0ydJ+lXQWic/qiIW12vCNgUEkcqBUSVjiRrivLtXKgikpBDaCE+ZQ4mFpWvY4RJjZTBcc65O0WyjyGH+x0P+g8+8EsxxjlxZmhxymZyQaGEajMeI3V5Pqus8uSWQh6cuQwgQVgRKTlYeJ2uR2PK143AS4hmmb3finbKmQzY7cVHenvlDEhq1pmm1Icu20wAiedJNbJ2Pb4yIGu3NFnYL65gMZZdSUJsVfKKqIKWXPaBkfl/X1ktUoPnmSi7h48U2LbNdljlmYExNiM81ojS+yIGbGML/GiUR2+DIiw0JZOkDUUzISWsGe/+synVaLADcvHMBNqAgGZEC7zJKKKzSHQSwktbB0sthuQ2AblglQBvRuL+aYKkKThvQjIaF6pR/CW9aVqnnE5uSBZmGaKS7LiYBHeEmGxfdbxUIlsC+6LCeEtpVYGCPqBgwW0mPWuIl5XBHmFEpPOrEmrisUutrUijFGeA1Bsm00xQxNjFtqxR2QZKG5y2LZa2eSBSBqCuoytCuvcBMzJBZhsOkE1yYzm+tiTlXqMmZkNTIngQXVALcQIeKprI0WV+X07AHJYlxVAh1j1JgDV1cEYHjwohOdtj0HNvB9BuCUOA1ZhPwDE2u5DyzAxbfUMArNhOaAJyrqpScxLx8VElkYsvCcuPPtBcYtZWHasR3pzJ+cECGMJrT9tnuCnxgFAsvSVmPEldwo40CJNivBJSat/3CrqlpB15i0wNJWsKQHoW3VJHLEOEzK1igAAADL1VQ8MWSJuFpDGYRv0tZPTssyAMaj+a2sPWSpwOYJGEPi4E2dM11eLI7AZMgDdNaqTT/NyLT54wU1DYzMSQqfLWQEM08QJK7OVadZclSbKLKFukkjsZUuFVTHeT5PQBWd5LcOPFPn5QsYEPW+uFOqPBpTJjYDxPLuMVFaxu0yxoiwTD1PAmuO/CppDK3U2OfGZBNTMfohqfEZkdEzqQkl9oRo6NlfA8aDNHBPsuLAUOIP1za2py4Bay0wJEaPA8s9YKHEQl1eA+bzxbGGHQX2wCVGlgau2JiYu2NkUS4G7OEBUx85w7F8412RZxclRiyungLLj90oisdwRSqCvcBj1vFi93nJxjbmj+ST7JklM78B9o4qc5/HNl9JL4Lo4C5Vl1Zi+DJJMo9t0QVRK9RGdqHhyZfYYU3uMjDUXvcbpTN7IlQkdRkB9mgCJ67X2yCyntUnR7COxJBQOCqxKqxX67SSmpa6Ft5ml2yMmJZxYEWWliV5aWXWZmfpW2Bj14y/CEsq7TGvDK43zd15XhkQ7DW6KLFVt9EAw1NCldqfZeKeKt+ZlbWfvumLrVvNG1myNZWtLAnYgfkvqBIr9hyrPUFuI8arS3NL7qwMi6U0sBxdoh84sMjqo22ISUAWuqQ0X/lo0CVnW+7Bw349ohXFQGJYIhiVWIW288iQjFHLanZeyBrsOTLH8SJNNbTECnwx/ESXJl/tE9oPdfZ8PRmp0VKnqQ5HOz9Unf/ZdbrKT0awSDKSMitzvDhsag5U14Jzx71sdXWCzNYbFhi6lhQFVuHldLPsHF2otW2448tZS0IGJxfLYsBKouPCrDtTK4Lu2ptqru6XVa6/Pe14PV9yyP851VQTBVYTSzZt2xG24bmUbf3XX7N0x8IBsCvEJZZHmmdewE6aBJxua7fL2h3CdXG8myQusSrWYcq3z6Z8natgt+9657peldSJ1ZEIsJJaSLUJOfN7bo8rEf7HHZuL5Kd2Rx5LUmUe6eW0/SCpbT2Iopjsrht4bCGDAFZEOqFar36a1DN2XJiW/Yh7LhjSNA2sptobTG/P6q4FYZpElpZ3XFx1Ix6S1KTooiEbQgwwq8zLvTug1w0N88PVQLEgW2jsfpqt3y4Z3faRXKcF7cAB8F4EEpjfdYTuLVCdzHuHx4kytz4QvZ6pXWdQmz+P+auTzkk1MxcHWErHpHmrTNZdYFdUeeyfxEVme8gg1rdySAgm2KPbDRgySQhgVULvpLj2MDrt33gr56HdR8W2pgmXklhMlXXKDha5wYATTVFAZZ3Ty24p6LIWZf5YzF8ltJuq+5ZbDIDovcMNZ92zFJ9gDx4MB1anbfpp9eYHpAMJf6T6wpzCxmXjr5Jamm2cvXcgxTp2dI68dN7uPYz5fR7zNsehLc3kPs9uGJ1+GDJt0psc/Gxz5zGEhS2w4XR7BrWzrBt6J0pFtxTYzRd+runPSiIea0/b5vGAURvaOrlbLHDdAIzHHZ/UrPSA7X9RXt8cK3eXsWj76baJgJAYWR/b/6K6vK1SeiehTtozmSXWoGbgMj+Wl3o2GdszVca6iF4co//Neni4USVkfixQbM8USc3Mdpud/QTOFlSn7XUPdEKJARAuwnQJ2f3KxTt7Pre68cj25qu9NxcoXKaogg1NezuPNe9sytvJtnuO4EGzO9tmrBOyzZzaRUDI4NrY+U7eMrb8qPbBjZPThevuuCOBUfmwQ8jlGxs/gyBtXri3xzmyE1DbGKpK9b+li+KjLbwb3apt4jZGUwEYWcNe+ugwodubm3iPqyByIgls62j2sK8dtRwhVR8f6n6SN/43ETpzP3KYn2s/rvTWlLCY55T1Mt1y/IWN4qjBdd21AxvCRagrxxHUySdwyLvOPhr5tcMIfnhmyaEgcPUEk3+E7OqxEskHhHw4vnJEyF1w3fewl/sej3PfA4XuewTTjQ+tuu8xX/c9GO3GR8nd+PC9+x5XeOMDHm98JOaNDxHdj129SKf/6izdex5U6x7tmwDqv5w7fMfDkF3Z/eT46L/axm9G9cOergAAAABJRU5ErkJggg==",
+           'accounts': [
+             {
+               'id': '1',
+               'name':'Expenses',
+               'type': 'checking'
+             },
+             {
+               'id': '2',
+               'name':'Savings',
+               'type': 'savings'
+             }
+           ]
+      }
+    ]
+      render(<UserContext.Provider value={{}}><BanksList allBanks={allBanks}/></UserContext.Provider>)
+     const deleteButton = screen.getByRole('button', /delete/i)
+     fireEvent.click(deleteButton)
+  })
+  
+    test('the prompt', () => {
+      const prompt = screen.getByRole('heading', { name: /delete this bank/i })
+      expect(prompt).toHaveTextContent("Are you sure you want to delete this bank?")
+    })
+
+    test('the yes button', () => {
+      const yesButton = screen.getByRole('button', { name: /yes/i })
+      expect(yesButton).not.toBeNull()
+    })
+
+    test('the no button', () => {
+      const noButton = screen.getByRole('button', { name: /no/i })
+      expect(noButton).not.toBeNull()
+    })
+
+    test('the no button and clicking it closes the modal', ()=> {
+      let noButton= screen.getByRole('button', { name: /no/i })
+      fireEvent.click(noButton)
+
+      const prompt = screen.queryByRole('heading', { name: /delete this bank/i })
+
+      const yesButton = screen.queryByRole('button', { name: /yes/i })
+      expect(yesButton).toBeNull()
+
+      noButton = screen.queryByRole('button', { name: /no/i })
+      expect(noButton).toBeNull()
+    })
+  })
+})
