cf2e588064224e3705edd0ee5d4b46cd6c653188
Gen Ohta <myself@genohta.com>
2020-12-09 22:24:32 -0800

    accounting for fixes costs prompt

--- src/components/SpendingPlan.js
+++ src/components/SpendingPlan.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 function SpendingPlan({ spendingPlan }) {
+  const { fixedCosts } = spendingPlan
   return (
     <div>
-    {
-      spendingPlan.map(spendingPlanCategory => {
-        const { category } = spendingPlanCategory
-        return (
-          <h1 key={category}>{category}</h1>
-        )
-      })
-    }
+      <h1>Fixed Costs</h1>
+      { 
+        fixedCosts ? null : <p>Looks like you haven't accounted for your fixed costs. Be sure to add them as parts of your spending plan</p>
+      }
+      <h1>Savings</h1>
+      <h1>Investments</h1>
+      <h1>Discretionary Spending</h1>
     </div>
   )
 }
--- src/components/__tests__/SpendingPlan.test.js
+++ src/components/__tests__/SpendingPlan.test.js
@@ -3,24 +3,45 @@ import { render, screen } from '@testing-library/react'
 import SpendingPlan from '../SpendingPlan'
 
 describe('When there is only one part to render, the SpendingPlan', () => {
-  beforeEach(() => {
-    const spendingPlan = [
-      {
-        category: 'Discretionary Spending',
-        parts: [
-          {
-            label: 'Spending Money',
-            searchTerm: '*',
-            expectedAmount: 0
-          }
-        ]
-      }
-    ]
-    render(<SpendingPlan spendingPlan={spendingPlan} />)
-  })
+  describe('renders', () => {
+    beforeEach(() => {
+      const spendingPlan = [
+        {
+          discretionarySpending: [
+            {
+              label: 'Spending Money',
+              searchTerm: '*',
+              expectedAmount: 0
+            }
+          ]
+        }
+      ]
+      render(<SpendingPlan spendingPlan={spendingPlan} />)
+    })
 
-  test('renders a discretionary spending header', () => {
-    const header = screen.getByRole('heading', { name: /discretionary spending/i})
-    expect(header).not.toBeNull()
+    test('a fixed costs header', () => {
+      const header = screen.getByRole('heading', { name: /fixed costs/i})
+      expect(header).not.toBeNull()
+    })
+
+    test('a message about not accounting for your fixed costs', () => {
+      const message = screen.getByText(/looks like you haven't accounted for your fixed costs\. be sure to add them as parts of your spending plan/i)
+      expect(message).not.toBeNull()
+    })
+
+    test('a savings header', () => {
+      const header = screen.getByRole('heading', { name: /savings/i})
+      expect(header).not.toBeNull()
+    })
+
+    test('a investments header', () => {
+      const header = screen.getByRole('heading', { name: /investments/i})
+      expect(header).not.toBeNull()
+    })
+
+    test('a discretionary spending header', () => {
+      const header = screen.getByRole('heading', { name: /discretionary spending/i})
+      expect(header).not.toBeNull()
+    })
   })
 })
