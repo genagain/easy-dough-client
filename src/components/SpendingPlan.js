7aa43ad610e8a0623ad6c29e2ec77dfb4fa374bd
Gen Ohta <myself@genohta.com>
2020-12-09 22:45:33 -0800

    spending plan with discretionary spending rendering

--- src/components/SpendingPlan.js
+++ src/components/SpendingPlan.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function SpendingPlan({ spendingPlan }) {
-  const { fixedCosts } = spendingPlan
+  const { fixedCosts, savings, investments, discretionarySpending} = spendingPlan
   return (
     <div>
       <h1>Fixed Costs</h1>
@@ -9,8 +9,25 @@ function SpendingPlan({ spendingPlan }) {
         fixedCosts ? null : <p>Looks like you haven't accounted for your fixed costs. Be sure to add them as parts of your spending plan</p>
       }
       <h1>Savings</h1>
+      {
+        savings ? null : <p>Looks like you aren't planning to save any money. Be sure to add savings as part of your spending plan</p>
+      }
       <h1>Investments</h1>
+      {
+        investments ? null : <p>Looks like you aren't planning to invest any money. Be sure to add investments as part of your spending plan</p>
+      }
       <h1>Discretionary Spending</h1>
+      <div>
+        <div>LABEL</div>
+        <div>SEARCH TERM</div>
+        <div>EXPECTED MONTHLY AMOUNT</div>
+      </div>
+      <div>
+        <div>{discretionarySpending.label}</div>
+        <div>{discretionarySpending.searchTerm}</div>
+        <div>${discretionarySpending.expectedAmount}</div>
+      </div>
+      
     </div>
   )
 }
--- src/components/__tests__/SpendingPlan.test.js
+++ src/components/__tests__/SpendingPlan.test.js
@@ -5,43 +5,77 @@ import SpendingPlan from '../SpendingPlan'
 describe('When there is only one part to render, the SpendingPlan', () => {
   describe('renders', () => {
     beforeEach(() => {
-      const spendingPlan = [
-        {
-          discretionarySpending: [
-            {
-              label: 'Spending Money',
-              searchTerm: '*',
-              expectedAmount: 0
-            }
-          ]
+      const spendingPlan = {
+          discretionarySpending: {
+            label: 'Spending Money',
+            searchTerm: '*',
+            expectedAmount: 0
+          }
         }
-      ]
       render(<SpendingPlan spendingPlan={spendingPlan} />)
     })
 
-    test('a fixed costs header', () => {
+    test('the fixed costs header', () => {
       const header = screen.getByRole('heading', { name: /fixed costs/i})
       expect(header).not.toBeNull()
     })
 
-    test('a message about not accounting for your fixed costs', () => {
+    test('the message about not accounting for your fixed costs', () => {
       const message = screen.getByText(/looks like you haven't accounted for your fixed costs\. be sure to add them as parts of your spending plan/i)
       expect(message).not.toBeNull()
     })
 
-    test('a savings header', () => {
+    test('the savings header', () => {
       const header = screen.getByRole('heading', { name: /savings/i})
       expect(header).not.toBeNull()
     })
 
-    test('a investments header', () => {
+    test('the message about not saving', () => {
+      const message = screen.getByText(/looks like you aren't planning to save any money\. be sure to add savings as part of your spending plan/i)
+      expect(message).not.toBeNull()
+    })
+
+    test('the investments header', () => {
       const header = screen.getByRole('heading', { name: /investments/i})
       expect(header).not.toBeNull()
     })
 
-    test('a discretionary spending header', () => {
+    test('the message about not investing', () => {
+      const message = screen.getByText(/looks like you aren't planning to invest any money\. be sure to add investments as part of your spending plan/i)
+      expect(message).not.toBeNull()
+    })
+
+    test('the discretionary spending header', () => {
       const header = screen.getByRole('heading', { name: /discretionary spending/i})
       expect(header).not.toBeNull()
+    })
+
+    test('the spending plan parts table headers', () => {
+      const expectedColumnHeaders = [
+        'LABEL',
+        'SEARCH TERM',
+        'EXPECTED MONTHLY AMOUNT'
+      ]
+
+      expectedColumnHeaders.forEach(columnHeader => {
+        const columnHeaderText = screen.getByText(columnHeader)
+        expect(columnHeaderText).not.toBeNull()
+      })
+    })
+
+    test('the spending money label', () => {
+      const label = screen.getByText(/spending money/i)
+      expect(label).not.toBeNull()
+    })
+
+    test('the * search term', () => {
+      const searchTerm = screen.getByText(/\*/i)
+      expect(searchTerm).not.toBeNull()
+    })
+
+    test('the $0 expected amount', () => {
+      const expectedAmount = screen.getByText(/\$0/i)
+      expect(expectedAmount).not.toBeNull()
     })
   })
 })
