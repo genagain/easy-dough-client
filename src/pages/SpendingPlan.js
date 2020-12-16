f66e2a03a6b3288a7e3d1191559729d5d690864e
Gen Ohta <myself@genohta.com>
2020-12-15 19:05:48 -0800

    rendering spending plan on the page

--- src/pages/SpendingPlan.js
+++ src/pages/SpendingPlan.js
@@ -1,18 +1,51 @@
 import React from 'react'
-//import SpendingPlan from '../components/SpendingPlan'
+import SpendingPlanCategories from '../components/SpendingPlanCategories'
 
 function SpendingPlan() {
   const spendingPlanCategories = {
+    fixedCosts: [
+      {
+        id: 1,
+        label: 'Rent',
+        searchTerm: 'Property Management Company',
+        expectedAmount: 1000
+      },
+      {
+        id: 2,
+        label: 'Electricity',
+        searchTerm: 'Electic Company',
+        expectedAmount: 40
+      },
+      {
+        id: 3,
+        label: 'Gas',
+        searchTerm: 'Gas Company',
+        expectedAmount: 20
+      },
+      {
+        id: 4,
+        label: 'Internet',
+        searchTerm: 'Internet Provider',
+        expectedAmount: 60
+      },
+      {
+        id: 5,
+        label: 'Groceries',
+        searchTerm: 'Grocery Store',
+        expectedAmount: 300
+      }
+    ],
     discretionarySpending: {
       label: 'Spending Money',
       searchTerm: '*',
       expectedAmount: 0
     }
   }
-    //<SpendingPlan spendingPlanCategories={spendingPlan} />
+
   return (
     <>
     <h1>Your Spending Plan</h1>
+    <SpendingPlanCategories spendingPlanCategories={spendingPlanCategories} />
     </>
   )
 }
