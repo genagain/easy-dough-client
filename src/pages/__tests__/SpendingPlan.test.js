20108a0a375794151f0ab93bf0ada6d39921fb62
Gen Ohta <myself@genohta.com>
2020-12-22 16:54:41 -0800

    fix test with providing a context

--- src/pages/__tests__/SpendingPlan.test.js
+++ src/pages/__tests__/SpendingPlan.test.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import SpendingPlan from '../SpendingPlan'
+import UserContext from '../../UserContext'
 
 describe('The SpendingPlan component', () => {
   describe('renders', () => {
     beforeEach(() => {
-      render(<SpendingPlan />)
+      const fakeAccessToken = 'fakeAccessToken'
+      render(<UserContext.Provider value={{ accessToken: fakeAccessToken }}><SpendingPlan /></UserContext.Provider>)
     })
 
     test('the Spending Plan header', () => {
