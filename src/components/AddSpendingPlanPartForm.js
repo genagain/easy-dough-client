9e2b1d5ed9586a01a7cd00bdc620f5e5db8234c5
Gen Ohta <myself@genohta.com>
2020-12-24 15:18:59 -0800

    style add spending plan part form

--- src/components/AddSpendingPlanPartForm.js
+++ src/components/AddSpendingPlanPartForm.js
@@ -36,13 +36,13 @@ function AddSpendingPlanPartForm({ category, refetch, setRefetch, setShowAddSpen
 
   return (
     <div>
-      <label htmlFor="label-input">Label:</label>
-      <input id="label-input" onChange={ e => setLabel(e.target.value) } />
-      <label htmlFor="label-search-term">Search Term:</label>
-      <input id="label-search-term" onChange={ e => setSearchTerm(e.target.value) } />
-      <label htmlFor="label-expected-amount">Expected Amount:</label>
-      <input id="label-expected-amount" onChange={ e => setExpectedAmount(e.target.value) }/>
-      <button onClick={handleAddSpendingPlanPart} >{ category === 'Savings' ? `Create Savings Fund`  : `Create ${category.slice(0,-1)}`}</button>
+      <label htmlFor="label-input" className="my-2 text-5xl lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg">Label:</label>
+      <input id="label-input" className="w-full lg:w-auto m-2 p-6 text-5xl border border-gray-400 rounded lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg" onChange={ e => setLabel(e.target.value) } />
+      <label htmlFor="label-search-term" className="my-2 text-5xl lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg">Search Term:</label>
+      <input id="label-search-term" className="w-full lg:w-auto m-2 p-6 text-5xl border border-gray-400 rounded lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg" onChange={ e => setSearchTerm(e.target.value) } />
+      <label htmlFor="label-expected-amount" className="my-2 text-5xl lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg">Expected Amount:</label>
+      <input id="label-expected-amount" className="w-full lg:w-auto m-2 p-6 text-5xl border border-gray-400 rounded lg:max-w-sm lg:my-4 lg:p-2 lg:text-lg" onChange={ e => setExpectedAmount(e.target.value) }/>
+      <button className="w-full lg:w-48 m-2 p-6 bg-blue-600 hover:bg-blue-500 text-white rounded-lg lg:my-2 lg:p-2 text-5xl lg:text-lg" onClick={handleAddSpendingPlanPart} >{ category === 'Savings' ? `Create Savings Fund`  : `Create ${category.slice(0,-1)}`}</button>
     </div>
   )
 }
