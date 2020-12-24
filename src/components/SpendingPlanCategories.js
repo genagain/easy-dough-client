1422b6d0aef543c2b278d9957b88f8791b5db28d
Gen Ohta <myself@genohta.com>
2020-12-23 20:45:40 -0800

    use investments

--- src/components/SpendingPlanCategories.js
+++ src/components/SpendingPlanCategories.js
@@ -16,30 +16,7 @@ function SpendingPlanCategories({ spendingPlanCategories }) {
       <h1 className="my-2 text-5xl lg:text-2xl">Savings</h1>
       <SpendingPlanCategory spendingPlanParts={savings} category="Savings" />
       <h1 className="my-2 text-5xl lg:text-2xl">Investments</h1>
-      {
-        investments ? (
-          <div className="border-gray-600 border rounded-lg lg:rounded lg:shadow">
-            <div className="flex flex-row bg-blue-800">
-              <div className="m-2 text-3xl w-3/12 lg:w-1/3 lg:text-sm text-white">LABEL</div>
-              <div className="m-2 text-3xl w-4/12 lg:w-1/3 lg:text-sm text-white">SEARCH TERM</div>
-              <div className="m-2 text-3xl w-5/12 lg:w-1/3 lg:text-sm text-white">EXPECTED AMOUNT</div>
-            </div>
-            {
-              investments.map(investment => {
-                const { id, label, searchTerm, expectedAmount } = investment
-                return (
-                  <div key={id} className="flex flex-row lg:items-baseline border-gray-600 border-t-2 lg:border-t">
-                    <div key={label} className="m-2 text-3xl w-3/12 lg:w-1/3 lg:m-2 lg:text-base">{label}</div>
-                    <div key={searchTerm} className="m-2 text-3xl w-4/12 lg:w-1/3 lg:m-2 lg:text-base">{searchTerm}</div>
-                    <div key={expectedAmount} className="m-2 text-3xl w-5/12 lg:w-1/3 lg:m-2 lg:text-base">${expectedAmount}</div>
-                  </div>
-                )
-              })
-            }
-          </div>
-        ): <p>Looks like you aren't planning to invest any money. Be sure to add investments as part of your spending plan.</p>
-      }
-      <button>Add Investment</button>
+      <SpendingPlanCategory spendingPlanParts={investments} category="Investments" />
       <h1 className="my-2 text-5xl lg:text-2xl">Discretionary Spending</h1>
       <div className="border-gray-600 border rounded-lg lg:rounded lg:shadow">
         <div className="flex flex-row bg-blue-800">
