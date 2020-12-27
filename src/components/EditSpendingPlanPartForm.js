701fbcef4704a48377d7ea152b6e9c5c3596a3d3
Gen Ohta <myself@genohta.com>
2020-12-26 21:55:16 -0800

    style edit spending plan part form

--- src/components/EditSpendingPlanPartForm.js
+++ src/components/EditSpendingPlanPartForm.js
@@ -3,15 +3,15 @@ import React from 'react'
 function EditSpendingPlanPartForm({part, setToggleForm}) {
   const { id, label: initialLabel, searchTerm: initialSearchTerm, expectedAmount: initialExpectedAmount } = part
   return (
-    <div>
-      <label htmlFor="label-input">Label:</label>
-      <input id="label-input" defaultValue={initialLabel} />
-      <label htmlFor="search-term-input">Search Term:</label>
-      <input id="search-term-input" defaultValue={initialSearchTerm} />
-      <label htmlFor="expected-amount-input">Expected Amount:</label>
-      <input id="expected-amount-input" defaultValue={initialExpectedAmount} />
-      <button>Update</button>
-      <button onClick={() => setToggleForm(false)}>Cancel</button>
+    <div className="flex flex-col m-auto w-3/4 lg:w-full lg:flex-row lg:items-center">
+      <label htmlFor="label-input" className="m-2 text-3xl lg:hidden">Label:</label>
+      <input id="label-input" className="w-full text-3xl m-2 p-4 border border-gray-400 rounded lg:text-base lg:w-1/3 lg:p-2" defaultValue={initialLabel} />
+      <label htmlFor="search-term-input" className="m-2 text-3xl lg:hidden">Search Term:</label>
+      <input id="search-term-input" className="w-full text-3xl m-2 p-4 border border-gray-400 rounded lg:text-base lg:w-1/3 lg:p-2" defaultValue={initialSearchTerm} />
+      <label htmlFor="expected-amount-input" className="m-2 text-3xl lg:hidden">Expected Amount:</label>
+      <input id="expected-amount-input" className="w-full text-3xl m-2 p-4 border border-gray-400 rounded lg:text-base lg:w-2/12 lg:p-2" defaultValue={initialExpectedAmount} />
+      <button className="w-full m-2 p-4 text-3xl bg-blue-600 text-white hover:bg-blue-500 rounded-lg lg:p-2 lg:w-1/12 lg:text-base">Update</button>
+      <button className="w-full m-2 p-4 text-3xl border border-blue-600 text-blue-600 hover:border-blue-500 lg:w-1/12 hover:text-blue-500 rounded-lg lg:p-2 lg:text-base" onClick={() => setToggleForm(false)}>Cancel</button>
     </div>
   )
 }
--- src/components/SpendingPlanCategories.js
+++ src/components/SpendingPlanCategories.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import SpendingPlanCategory from './SpendingPlanCategory'
 import SpendingPlanPartRow from './SpendingPlanPartRow'
-
 function SpendingPlanCategories({ spendingPlanCategories, refetch, setRefetch }) {
   const { fixedCosts, savings, investments, discretionarySpending} = spendingPlanCategories
 
@@ -18,9 +17,9 @@ function SpendingPlanCategories({ spendingPlanCategories, refetch, setRefetch })
       <h1 className="my-2 text-5xl lg:text-2xl">Discretionary Spending</h1>
       <div className="border-gray-600 border rounded-lg lg:rounded lg:shadow">
         <div className="flex flex-row bg-blue-800">
-          <div className="m-2 text-white w-3/12 text-3xl lg:text-sm">LABEL</div>
-          <div className="m-2 text-white w-4/12 text-3xl lg:text-sm">SEARCH TERM</div>
-          <div className="m-2 text-white w-5/12 text-3xl lg:text-sm">EXPECTED AMOUNT</div>
+          <div className="m-2 text-white w-3/12 lg:w-1/3 text-3xl lg:text-sm">LABEL</div>
+          <div className="m-2 text-white w-4/12 lg:w-1/3 text-3xl lg:text-sm">SEARCH TERM</div>
+          <div className="m-2 text-white w-5/12 lg:w-1/3 text-3xl lg:text-sm">EXPECTED AMOUNT</div>
        </div>
        <SpendingPlanPartRow part={discretionarySpending} />
       </div>
