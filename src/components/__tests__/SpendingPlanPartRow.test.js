7a6e0b51300be43c4c22cfe68d677fa6b71ce2e4
Gen Ohta <myself@genohta.com>
2020-12-27 14:58:49 -0800

    update spending plan part row for fixed costs

--- src/components/__tests__/SpendingPlanPartRow.test.js
+++ src/components/__tests__/SpendingPlanPartRow.test.js
@@ -42,3 +42,42 @@ describe('the SpendingPlanPartRow component for discretionary spending', () => {
     })
   })
 })
+
+describe('the SpendingPlanPartRow component for fixed costs', () => {
+  beforeEach(() => {
+    const part = {
+      id: 2,
+      label: 'Rent',
+      searchTerm: 'Property Management',
+      expectedAmount: 1000,
+    }
+    render(<SpendingPlanPartRow part={part} category={'Fixed Costs'}/>)
+  })
+
+  describe('renders', () => {
+    test('the rent label', () => {
+      const label = screen.getByText(/rent/i)
+      expect(label).not.toBeNull()
+    })
+
+    test('the property management search term', () => {
+      const searchTerm = screen.getByText(/property management/i)
+      expect(searchTerm).not.toBeNull()
+    })
+
+    test('the $0 expected amount', () => {
+      const expectedAmount = screen.getByText(/\$1000/i)
+      expect(expectedAmount).not.toBeNull()
+    })
+
+    test('the edit button', () => {
+      const button = screen.getByRole('button', { name: /edit/i })
+      expect(button).not.toBeNull()
+    })
+
+    test('the delete button', () => {
+      const button = screen.getByRole('button', { name: /delete/i })
+      expect(button).not.toBeNull()
+    })
+  })
+})
