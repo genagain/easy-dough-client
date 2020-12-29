7e7cc08f891cffd82a70508d526b1dddcc1909e8
Gen Ohta <myself@genohta.com>
2020-12-29 14:22:27 -0800

    format transaction table headers and rows

--- src/components/TransactionsTable.js
+++ src/components/TransactionsTable.js
@@ -7,8 +7,8 @@ function TransactionsTable({transactions}) {
           <div className="border-b-2 border-gray-600 lg:border-none">
             <div className="flex flex-row invisible lg:visible lg:bg-blue-800">
               <div className="m-2 lg:text-sm lg:w-40 lg:text-white">DATE</div>
-              <div className="m-2 lg:text-sm lg:w-84 lg:text-white">DESCRIPTION</div>
-              <div className="m-2 lg:text-sm lg:w-84 lg:text-white">LABEL</div>
+              <div className="m-2 lg:text-sm lg:w-72 lg:text-white">DESCRIPTION</div>
+              <div className="m-2 lg:text-sm lg:w-72 lg:text-white">LABEL</div>
               <div className="m-2 lg:text-sm lg:w-96 lg:text-white">AMOUNT</div>
             </div>
           </div>
