3ecffc2a3f81ec2d49bb8563eb7c41733909ff9b
Gen Ohta <myself@genohta.com>
2020-12-30 19:51:57 -0800

    render stacked horizontal bar chart

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,7 +1,49 @@
-import React from 'react';
-
+import React, {useEffect} from 'react';
+import Chart from 'chart.js';
+ /*       options: {*/
+          //scales: {
+            //xAxes: [{
+              //stacked: true
+            /*}]*/
+          //}
 function Report(props) {
-  return <h1>hey</h1>
+  useEffect(() => {
+		var ctx = document.getElementById('myChart').getContext('2d');
+		var chart = new Chart(ctx, {
+				// The type of chart we want to create
+        type: 'horizontalBar',
+
+				// The data for our dataset
+				data: {
+						labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+						datasets: [{
+								label: 'My First dataset',
+								backgroundColor: 'rgb(255, 99, 132)',
+								borderColor: 'rgb(255, 99, 132)',
+								data: [20, 10, 5, 2, 20, 30, 45]
+						}, {
+								label: 'My Second dataset',
+								backgroundColor: 'rgb(255, 132, 99)',
+								borderColor: 'rgb(255, 132, 99)',
+								data: [0, 10, 12, 42, 21, 34, 11]
+						}]
+				},
+
+				// Configuration options go here
+
+      options: {
+        scales: {
+          xAxes: [{
+            stacked: true
+          }],
+          yAxes: [{
+            stacked: true
+          }]
+        }
+      }
+		});
+  }, [])
+  return <canvas id="myChart"></canvas>
 }
 
 export default Report
