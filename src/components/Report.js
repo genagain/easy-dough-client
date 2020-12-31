b9373e383cc8209b6745aab56c7617cd9f5b49db
Gen Ohta <myself@genohta.com>
2020-12-30 19:58:42 -0800

    render squares overlapping

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,49 +1,18 @@
 import React, {useEffect} from 'react';
 import Chart from 'chart.js';
- /*       options: {*/
-          //scales: {
-            //xAxes: [{
-              //stacked: true
-            /*}]*/
-          //}
+
+			//<rect height="50" width="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)"></rect>
 function Report(props) {
-  useEffect(() => {
-		var ctx = document.getElementById('myChart').getContext('2d');
-		var chart = new Chart(ctx, {
-				// The type of chart we want to create
-        type: 'horizontalBar',
-
-				// The data for our dataset
-				data: {
-						labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-						datasets: [{
-								label: 'My First dataset',
-								backgroundColor: 'rgb(255, 99, 132)',
-								borderColor: 'rgb(255, 99, 132)',
-								data: [20, 10, 5, 2, 20, 30, 45]
-						}, {
-								label: 'My Second dataset',
-								backgroundColor: 'rgb(255, 132, 99)',
-								borderColor: 'rgb(255, 132, 99)',
-								data: [0, 10, 12, 42, 21, 34, 11]
-						}]
-				},
-
-				// Configuration options go here
-
-      options: {
-        scales: {
-          xAxes: [{
-            stacked: true
-          }],
-          yAxes: [{
-            stacked: true
-          }]
-        }
-      }
-		});
-  }, [])
-  return <canvas id="myChart"></canvas>
+  return (
+		<svg width="700" height="200">
+			<rect height="100" width="600" style={{
+       fill: 'rgb(100,100,100)'
+       }}></rect>
+			<rect height="100" width="50" style={{
+       fill: 'rgb(255,0,255)'
+       }}></rect>
+    </svg>
+  )
 }
 
 export default Report
