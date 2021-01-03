ea6b126a9610bd0894aacdae74e92ef15484b103
Gen Ohta <myself@genohta.com>
2021-01-02 19:18:18 -0800

    make bars wider

--- src/components/ReportRow.js
+++ src/components/ReportRow.js
@@ -3,7 +3,7 @@ import { formatAmount } from '../utils'
 
 function ReportRow({ label, actualAmount, expectedAmount, difference}) {
   const intViewportWidth = window.innerWidth
-  const rowWidth = intViewportWidth > 1000 ? 700 : 500
+  const rowWidth = intViewportWidth > 1000 ? 1000 : 500
   const rowHeight = intViewportWidth > 1000 ? 25 : 50
 
   const formattedActualAmount = formatAmount(actualAmount)
@@ -41,14 +41,14 @@ function ReportRow({ label, actualAmount, expectedAmount, difference}) {
   }
 
   return ( 
-    <div className="m-auto w-2/3 lg:w-3/4 p-2">
+    <div className="m-auto p-2">
       <h1 className="text-5xl lg:text-xl">{label}</h1>
 			<div className="text-3xl lg:text-base">Spent ${formattedActualAmount} of ${formattedExpectedAmount} </div>
 			<div className="flex flex-col lg:flex-row lg:items-center">
 				<div className="lg:flex-grow">
 				{ bar }
 				</div>
-			<div className="text-3xl lg:text-base">${formattedDifference} {spentVerb}</div>
+			<div className="text-3xl lg:text-lg">${formattedDifference} {spentVerb}</div>
 			</div>
     </div>
   )
