4ed62aba487ee9da01c56cdba8045483d3a14bf3
Gen Ohta <myself@genohta.com>
2021-01-02 19:08:30 -0800

    not send a request when there is no report param

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -36,6 +36,9 @@ function Reports() {
 
   useEffect(() => {
     const fetchHistoricalSpending = async () => {
+      if (!reportParam) {
+        return
+      }
       const apiUrl = process.env.REACT_APP_SERVER_BASE_URL
       const response = await fetch(`${apiUrl}/reports/generate?month=${reportParam}`,
         {
@@ -58,39 +61,8 @@ function Reports() {
 
     fetchHistoricalSpending()
 
-  }, [reportParam])
+  }, [reportParam, accessToken, logout])
 
-  /*const historicalSpending = [*/
-    //{
-      //label: 'Debt',
-      //actualAmount: 910.8,
-      //expectedAmount: 910.8,
-      //difference: 0
-    //},
-    //{
-      //label: 'Cellphone',
-      //actualAmount: 40,
-      //expectedAmount: 40,
-      //difference: 0 //},
-    //{
-      //label: 'Groceries',
-      //actualAmount: 300,
-      //expectedAmount: 400,
-      //difference: 100
-    //},
-    //{
-      //label: 'Spending Money',
-      //actualAmount: 550,
-      //expectedAmount: 436.29,
-      //difference: 113.71
-    //}
-  //]
-
-  //const months = [
-    //'January',
-    //'February',
-    //'March'
-  /*]*/
   return (
     <div className="m-auto w-10/12 lg:max-w-6xl">
       <Report historicalSpending={historicalSpending} months={months} setReportParam={setReportParam}/>
