f233e752fd9e7445b6507215b100744c0c98bca2
Gen Ohta <myself@genohta.com>
2021-01-24 00:50:33 -0800

    replace images

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -12,7 +12,7 @@ function Index({ history }) {
               <p className="text-2xl text-blue-800">Create a plan. Track your spending automagically.</p>
               <button className="p-6 bg-blue-800 hover:bg-blue-700 text-white rounded-lg lg:p-2 text-5xl lg:text-xl" onClick={() => history.push('/signup')}>Sign Up</button>
             </div>
-            <img className="h-120 w-120" src="images/money.svg" />
+            <img alt="Piggy Bank" className="h-120 w-120" src="images/money.svg" />
           </div>
         </div>
       </div>
@@ -20,7 +20,7 @@ function Index({ history }) {
         <div className="flex flex-col py-16">
           <div className="flex flex-row items-center">
             <div className="w-1/2">
-              <img className="h-84 w-84" src="images/bank_accounts.svg" />
+              <img  alt="Online Banking" className="h-84 w-84" src="images/bank_accounts.svg" />
             </div>
             <div className="w-1/2 flex flex-col flex-grow items-start space-y-6">
               <h1 className="text-3xl text-blue-800 font-semibold">Connect all your accounts</h1>
@@ -35,14 +35,14 @@ function Index({ history }) {
               <p className="text-xl text-blue-800">Plan your spending so you can save easily.</p>
             </div>
             <div className="w-1/2">
-              <img className="h-84 w-84" src="images/spending_plan.svg" />
+              <img alt="Spending Plan" className="h-84 w-84" src="images/spending_plan.svg" />
             </div>
           </div>
         </div>
         <div className="flex flex-col py-16">
           <div className="flex flex-row items-center">
             <div className="w-1/2">
-              <img className="h-84 w-84" src="images/transactions.svg" />
+              <img alt="Track Spending" className="h-84 w-84" src="images/transactions.svg" />
             </div>
             <div className="w-1/2 flex flex-col flex-grow items-start space-y-6">
               <h1 className="text-3xl text-blue-800 font-semibold">Track your spending</h1>
@@ -57,7 +57,7 @@ function Index({ history }) {
               <p className="text-xl text-blue-800">Visualize your spending easily to stick to your plan.</p>
             </div>
             <div className="w-1/2">
-              <img className="h-84 w-84" src="images/reports.svg" />
+              <img alt="Visualize Spending" className="h-84 w-84" src="images/reports.svg" />
             </div>
           </div>
         </div>
