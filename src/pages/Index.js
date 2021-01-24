ea081e817fa03e618e7105f7c9048368a643b572
Gen Ohta <myself@genohta.com>
2021-01-24 01:13:44 -0800

    Landing page (#35)
    * generate more widths
    
    * add images
    
    * create landing page
    
    * refactor markup
    
    * replace images
    
    * ensure the mark up is correct
    
    * test index page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,9 +1,69 @@
 import React from 'react'
-
-function Index() {
+import { withRouter } from 'react-router-dom';
+ 
+function Index({ history }) {
   return (
-      <h1>Index</h1>
+    <>
+      <div className="-mt-6 flex flex-col bg-gray-100 py-16">
+        <div className="m-auto w-10/12 lg:max-w-6xl">
+          <div className="flex flex-row items-center">
+            <div className="flex flex-col flex-grow items-start space-y-6">
+              <h1 className="text-5xl text-blue-800 font-semibold">Manage Your Money Easily</h1>
+              <p className="text-2xl text-blue-800">Create a plan. Track your spending automagically.</p>
+              <button className="p-6 bg-blue-800 hover:bg-blue-700 text-white rounded-lg lg:p-2 text-5xl lg:text-xl" onClick={() => history.push('/signup')}>Sign Up</button>
+            </div>
+            <img alt="Piggy Bank" className="h-120 w-120" src="images/money.svg" />
+          </div>
+        </div>
+      </div>
+      <div className="m-auto w-10/12 lg:max-w-6xl">
+        <div className="flex flex-col py-16">
+          <div className="flex flex-row items-center">
+            <div className="w-1/2">
+              <img  alt="Online Banking" className="h-84 w-84" src="images/bank_accounts.svg" />
+            </div>
+            <div className="w-1/2 flex flex-col flex-grow items-start space-y-6">
+              <h1 className="text-3xl text-blue-800 font-semibold">Connect all your accounts</h1>
+              <p className="text-xl text-blue-800">Easily add all of your bank accounts and credit cards.</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col py-16">
+          <div className="flex flex-row items-center">
+            <div className="w-1/2 flex flex-col flex-grow items-start space-y-6">
+              <h1 className="text-3xl text-blue-800 font-semibold">Create a spending plan</h1>
+              <p className="text-xl text-blue-800">Plan your spending so you can save easily.</p>
+            </div>
+            <div className="w-1/2">
+              <img alt="Spending Plan" className="h-84 w-84" src="images/spending_plan.svg" />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col py-16">
+          <div className="flex flex-row items-center">
+            <div className="w-1/2">
+              <img alt="Track Spending" className="h-84 w-84" src="images/transactions.svg" />
+            </div>
+            <div className="w-1/2 flex flex-col flex-grow items-start space-y-6">
+              <h1 className="text-3xl text-blue-800 font-semibold">Track your spending</h1>
+              <p className="text-xl text-blue-800">Automatically label transactions to easily track your spending.</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col py-16">
+          <div className="flex flex-row items-center">
+            <div className="w-1/2 flex flex-col flex-grow items-start space-y-6">
+              <h1 className="text-3xl text-blue-800 font-semibold">Stay on track</h1>
+              <p className="text-xl text-blue-800">Visualize your spending easily to stick to your plan.</p>
+            </div>
+            <div className="w-1/2">
+              <img alt="Visualize Spending" className="h-84 w-84" src="images/reports.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Index
+export default withRouter(Index)
--- src/pages/__tests__/Index.test.js
+++ src/pages/__tests__/Index.test.js
@@ -1,10 +1,50 @@
 import React from 'react';
-import {render} from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import {screen, render} from '@testing-library/react'
 import Index from '../Index'
 
-test('render Index page', () => {
-  const testText = 'index'
-  const { getByRole } = render(<Index />)
-  const title = getByRole("heading", { name: /index/i});
-  expect(title).toHaveTextContent("Index")
+describe('The Index component', () => {
+  describe('renders', () => {
+    beforeEach(() => {
+      render(<BrowserRouter><Index /></BrowserRouter>)
+    })
+
+    test('the short value proposition', () => {
+      const valueProp = screen.getByRole("heading", { name: /manage your money easily/i});
+      expect(valueProp).toHaveTextContent('Manage Your Money Easily')
+    })
+
+    test('the long value proposition', () => {
+      const valueProp = screen.getByText('Create a plan. Track your spending automagically.')
+      expect(valueProp).not.toBeNull()
+    })
+
+    test('each feature header', () => {
+      const expectedHeaders = [
+        "Connect all your accounts",
+        "Create a spending plan",
+        "Track your spending",
+        "Stay on track"
+      ]
+
+      expectedHeaders.forEach(expectedHeader => {
+        const header = screen.getByRole("heading", { name: expectedHeader })
+        expect(header).not.toBeNull()
+      })
+    })
+
+    test('each feature explanation', () => {
+      const expectedExplainations = [
+        "Easily add all of your bank accounts and credit cards.",
+        "Plan your spending so you can save easily.",
+        "Automatically label transactions to easily track your spending.",
+        "Visualize your spending easily to stick to your plan."
+      ]
+
+      expectedExplainations.forEach(expectedExplaination => {
+        const explaination = screen.getByText(expectedExplaination)
+        expect(explaination).not.toBeNull()
+      })
+    })
+  })
 })
