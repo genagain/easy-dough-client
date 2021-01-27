11aaf402e8104f74f95c7c0e48d02768c430d2fc
Gen Ohta <myself@genohta.com>
2021-01-26 23:04:24 -0800

    Sign up required fields (#36)
    * refactor sign up page to use an array of errors
    
    * style auth form
    
    * style error message
    
    * validate all fields

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -11,19 +11,28 @@ function Signup() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirmation, setPasswordConfirmation] = useState('')
-  const [error, setError] = useState('')
+  const [errors, setErrors] = useState([])
 
   async function handleSubmit(event) {
     event.preventDefault()
+    setErrors([])
+
+    const allFields = [firstname, lastname, email, password, passwordConfirmation]
+
+    allFields.forEach(field => {
+      if (field === '') {
+        setErrors(['Please fill out all of the fields'])
+      }
+    })
 
     let validEmail = /^[A-Za-z0-9_.]+@\w+.\w+.\w+/
     if(!validEmail.test(email)) {
-      setError('Please provide a valid email')
+      setErrors(prevErrors => [...prevErrors, 'Please provide a valid email'])
       return
     }
 
-    if (password !== passwordConfirmation) {
-      setError('Please make ensure the passwords provided match')
+    if (password !== passwordConfirmation || password === '') {
+      setErrors(prevErrors => [...prevErrors, 'Please ensure the passwords provided match'])
       return
     }
 
@@ -34,19 +43,26 @@ function Signup() {
       body: JSON.stringify({ firstname, lastname, email, password})
     })
 
+    setErrors([])
     if (response.ok) {
       history.push('/login')
     } else {
       const json = await response.json()
-      setError(json['message'])
+      setErrors([json['message']])
     }
   }
 
   return (
     <div className="flex flex-col">
-    <h4 className="m-auto text-6xl lg:text-4xl">Sign Up</h4>
-    {error ? <p>{error}</p> : null}
-    <AuthForm isSignup onSubmit={handleSubmit} fieldSetters={{setFirstname, setLastname, setEmail, setPassword, setPasswordConfirmation}}/>
+      <h4 className="m-auto text-6xl lg:text-4xl">Sign Up</h4>
+      <div className="w-2/3 lg:w-1/3 m-auto">
+      {
+        errors.map(error => (
+        <p className="w-fit-content m-auto text-5xl lg:text-lg text-red-600" key={error}>{error}</p>
+        ))
+      }
+      <AuthForm isSignup onSubmit={handleSubmit} fieldSetters={{setFirstname, setLastname, setEmail, setPassword, setPasswordConfirmation}}/>
+      </div>
     </div>
   )
 }
