98339602f5b3c55b343d2b9318d8e34803296204
Gen Ohta <myself@genohta.com>
2021-02-18 18:55:00 -0800

    fix wonky table on smaller screens

--- src/components/TransactionsTable.js
+++ src/components/TransactionsTable.js
@@ -6,9 +6,11 @@ function TransactionsTable({transactions}) {
         <div className="lg:border-gray-600 lg:border lg:rounded lg:shadow">
           <div className="border-b-2 border-gray-600 lg:border-none">
             <div className="flex flex-row invisible lg:visible lg:bg-blue-800">
-              <div className="m-2 lg:text-sm lg:w-40 lg:text-white">DATE</div>
-              <div className="m-2 lg:text-sm lg:w-72 lg:text-white">DESCRIPTION</div>
-              <div className="m-2 lg:text-sm lg:w-72 lg:text-white">LABEL</div>
+              <div className="lg:flex lg:flex-row lg:flex-grow-0">
+                <div className="m-2 lg:text-sm lg:w-40 lg:text-white">DATE</div>
+                <div className="m-2 lg:text-sm lg:w-72 lg:text-white">DESCRIPTION</div>
+                <div className="m-2 lg:text-sm lg:w-72 lg:text-white">LABEL</div>
+              </div>
               <div className="m-2 lg:text-sm lg:w-96 lg:text-white">AMOUNT</div>
             </div>
           </div>
